350e7c2596cc12f1fdad9712fff2aab6423fe57d
Gabo Esquivel <contact@gaboesquivel.com>
2018-09-14 21:37:37 -0600

    add basic get_producers cron job

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,36 @@
-const fastify = require("fastify");
-const app = fastify();
+const fastify = require('fastify')
+// import { graphiqlFastify, graphqlFastify } from 'fastify-graphql'
+const humanToCron = require('human-to-cron')
+const schedule = require('node-schedule')
+const eosCamelApi = require('@eoscostarica/eosjs-camel-api')
 
-const { graphiqlFastify, graphqlFastify } = require("fastify-graphql");
-app.register(graphqlFastify, {
-  prefix: "/graphql",
-  graphql: {
-    schema: your_graphql_schema
+const eosApi = eosCamelApi.getInstance()
+
+const syncProducers = async () => {
+  try {
+    const info = await eosApi.getProducers({})
+    console.log('===========================')
+    console.log(`Got producers`)
+    console.log(info)
+  } catch (err) {
+    console.log(err)
   }
-});
-app.register(graphiqlFastify, {
-  prefix: "/graphiql",
-  graphiql: {
-    endpointURL: "/graphql"
-  }
-});
+}
+
+const timeInterval = humanToCron('once each second')
+const SyncProducersJob = schedule.scheduleJob(timeInterval, syncProducers)
+
+const app = fastify()
+
+// app.register(graphqlFastify, {
+//   prefix: "/graphql",
+//   graphql: {
+//     schema: your_graphql_schema
+//   }
+// });
+// app.register(graphiqlFastify, {
+//   prefix: "/graphiql",
+//   graphiql: {
+//     endpointURL: "/graphql"
+//   }
+// });
