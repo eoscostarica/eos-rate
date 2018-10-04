400ce9d70c152b20294cc7fcb24db07e2bc237b5
Gabo Esquivel <contact@gaboesquivel.com>
2018-10-03 23:44:20 -0600

    refactor(common): comply to standardjs (#38)

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { translate } from 'react-i18next'
+import PropTypes from 'prop-types'
 
 const Account = ({ t }) => <h1>{t('accountTitle')}</h1>
+
+Account.propTypes = {
+  t: PropTypes.object.isRequired
+}
 
 export default translate('translations')(Account)
