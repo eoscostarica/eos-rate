94f49e430f559c4f3121c93c54c0b664e59a7733
Daniel Prado <danazkari@gmail.com>
2018-10-10 21:52:27 -0600

    fix(slider): add tooltips to param sliders (#48)
    Adds tooltips to the parameters range sliders on the filters (and anywhere where those filters are being used)

--- src/components/parameter-range-selector/index.js
+++ src/components/parameter-range-selector/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import { Range } from 'rc-slider'
+import Slider, { createSliderWithTooltip } from 'rc-slider'
 
 import './style.css'
+
+const Range = createSliderWithTooltip(Slider.Range)
 
 const styles = {
   root: {}
@@ -13,7 +15,10 @@ const ParameterRangeSelector = ({ classes, theme, ...props }) => (
   <Range
     className={classes.root}
     pushable={10}
+    min={0}
+    max={100}
     trackStyle={[{ backgroundColor: theme.palette.secondary.dark }]}
+    tipFormatter={value => `${value ? value / 10 : 0}`}
     handleStyle={[
       {
         backgroundColor: theme.palette.secondary.main,
