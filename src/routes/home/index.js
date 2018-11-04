b67752f40cc31234b36e3494ec041ab3312feb51
Mariano Alvarez <malvarezhcr@gmail.com>
2018-11-04 00:39:56 -0600

    feat(home): create home page

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,72 @@
-import React from 'react'
-import { translate } from 'react-i18next'
+import React, { Component } from 'react'
+import { Redux } from 'redux-render'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 import PropTypes from 'prop-types'
 
-const Home = ({ t }) => <h1>{t('homeTitle')}</h1>
+import Cover from './cover'
+import SubTopic from './subTopic'
+import store from 'store'
 
-Home.propTypes = {
-  t: PropTypes.func.isRequired
+const { dispatch } = store
+
+const styles = ({ spacing, palette }) => ({
+  spacingContainers: {
+    padding: spacing.unit * 4
+  },
+  coverContainer: {
+    backgroundColor: palette.primary.dark
+  },
+  subTopicContainer: {
+    backgroundColor: '#1a1b29'
+  }
+})
+
+class Home extends Component {
+  componentDidMount () {
+    dispatch.home.getBlockData()
+  }
+
+  render () {
+    const { classes } = this.props
+    return (
+      <Redux
+        selector={state => ({
+          home: state.home
+        })}
+      >
+        {({ home }) => (
+          <Grid container direction='column'>
+            <Grid item xs>
+              {home.blockProducer && (
+                <div
+                  className={`${classes.spacingContainers}
+                    ${classes.coverContainer}
+                  `}
+                >
+                  <Cover blockProducer={home.blockProducer} />
+                </div>
+              )}
+            </Grid>
+
+            <Grid item xs>
+              <div
+                className={`${classes.spacingContainers}
+                ${classes.subTopicContainer}
+              `}
+              >
+                <SubTopic />
+              </div>
+            </Grid>
+          </Grid>
+        )}
+      </Redux>
+    )
+  }
 }
 
-export default translate('translations')(Home)
+Home.propTypes = {
+  classes: PropTypes.object.required
+}
+
+export default withStyles(styles)(Home)
