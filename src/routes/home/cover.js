47c5f118520c4515cf4257997cf5ddf685ea771d
Mariano Alvarez <malvarezhcr@gmail.com>
2018-11-04 11:57:17 -0600

    feat(home): create home page (#69)
    This PR implement the home page. 
    
    Things to consider:
    - There is a wrapper setting a fix padding to the main tag. Ticket: #67
    - Nav should not be visible in the home page. Ticket: #68
    - Ranger component should be update to work as slider too.
    
    # Changes
    1. Create Video component placeholder
    2. Modify material ui button styles theme
    3. Create home components
    
    # Steps to test
    1. `git remote add mahcr git@github.com:mahcr/eos-rate.git`
    2. `git checkout home-page`
    3. Run the project and go to `localhost:3000`
    
    <img width="1418" alt="screen shot 2018-11-04 at 1 16 55 am" src="https://user-images.githubusercontent.com/16544451/47961370-5e698c80-dfcf-11e8-9228-f3317989ff28.png">

---
+++ src/routes/home/cover.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { withNamespaces } from 'react-i18next'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import ParameterRangeSelector from 'components/parameter-range-selector'
+import BlockProducerRadar from 'components/block-producer-radar'
+import bpParameters from 'config/comparison-parameters'
+
+const styles = ({ palette, typography }) => ({
+  coverContainer: {
+    color: '#fff'
+  },
+  title: {
+    fontSize: typography.h4.fontSize
+  },
+  paragraph: {
+    color: palette.grey[600]
+  },
+  ctaContainer: {
+    textAlign: 'right'
+  },
+  chartContainer: {
+    maxWidth: '400px',
+    width: '100%'
+  }
+})
+
+const HomeCover = ({ classes, t, blockProducer }) => (
+  <Grid item container xs={12} className={classes.coverContainer} spacing={16}>
+    <Grid item container xs={12} md={6} justify='center'>
+      <div className={classes.chartContainer}>
+        <BlockProducerRadar
+          bpData={{
+            labels: bpParameters,
+            datasets: [blockProducer.data]
+          }}
+        />
+      </div>
+    </Grid>
+
+    <Grid item xs={12} md={6}>
+      <Typography variant='headline' className={classes.title}>
+        {t('cover.title')}
+      </Typography>
+
+      <Typography
+        variant='body2'
+        className={classes.paragraph}
+        align='justify'
+        paragraph
+      >
+        {t('cover.paragraph')}
+      </Typography>
+
+      <div className={classes.ctaContainer}>
+        <Button
+          className='textPrimary'
+          variant='contained'
+          size='small'
+          color='secondary'
+        >
+          {t('cover.cta')}
+        </Button>
+      </div>
+    </Grid>
+
+    <Grid item xs={12}>
+      <ParameterRangeSelector defaultValue={[0, 50]} />
+    </Grid>
+  </Grid>
+)
+
+HomeCover.propTypes = {
+  t: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+  blockProducer: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(withNamespaces('home')(HomeCover))
