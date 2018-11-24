715b32d479d42f932274326cf14166d04a559bea
rubenabix <rubenabix@gmail.com>
2018-11-23 18:37:47 -0600

    feature: add rate basic contract
    Former-commit-id: d6b8d1ac680104e2a5957bb20037f5a47aea8706

--- contracts/eosrate/eosrate.cpp
+++ contracts/eosrate/eosrate.cpp
@@ -2,14 +2,44 @@
 #include <eosiolib/print.hpp>
 
 using namespace eosio;
+using std::string;
 
 CONTRACT eosrate : public contract {
-  public:
-      using contract::contract;
+public:
+    using contract::contract;
 
-      ACTION rate( name user ) {
-         require_auth( user );
-         print( "Thanks for rating my friend, ", name{user});
-      }
+    eosrate(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+
+    ACTION upsert(name rater_account, const string json) {
+        require_auth(rater_account);
+        // Quick check to remind the user the payload must be json.
+        eosio_assert(json[0] == '{',             "payload must be json");
+        eosio_assert(json[json.size()-1] == '}', "payload must be json");
+        rate_index rates(_code, _code.value);
+
+        auto iterator = rates.find(rater_account.value);
+        if( iterator == rates.end() )
+        {
+            rates.emplace(rater_account, [&]( auto& row ) {
+                row.key = rater_account;
+                row.json = json;
+            });
+        }
+        else {
+            rates.modify(iterator, rater_account, [&]( auto& row ) {
+                row.json = json;
+            });
+        }
+    }
+
+
+private:
+    TABLE rate {
+        name key;
+        string json;
+        uint64_t primary_key() const { return key.value; }
+    };
+    typedef eosio::multi_index<"rates"_n, rate> rate_index;
 };
-EOSIO_DISPATCH( eosrate, (rate))
+
+EOSIO_DISPATCH(eosrate, (upsert));
