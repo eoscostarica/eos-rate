20b89ea6c5ebb80173d2823b40fca293ab1b629a
Daniel Prado <danazkari@gmail.com>
2018-11-24 10:51:32 -0600

    refactor(webapp): trim profile page code down, add redirect to not-found (#102)
    This quite a refactor, please be thorough when looking at the code changes.
    
    ### Steps to test
    1. Go into block producers page.
    1. Select a block-producer profile page.
    1. Look at the redux state, it shouldn't have any change related to loading the profile page.
    1. Profile page should look exactly as production does.
    1. Try to navigate to a non existent account like `/block-producers/eostest`.
    1. You should get redirected to the `Not found` page.
    1. Hitting back, should return you to the last valid address.
    
    Former-commit-id: a24b01471c98031c4d7e5cc8f5bcaa4ec26179ce

--- services/frontend/src/routes/block-producers/block-producer-profile.js
+++ services/frontend/src/routes/block-producers/block-producer-profile.js
@@ -1,37 +1,325 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Redux } from 'redux-render'
-import { withStyles } from '@material-ui/core/styles'
 import { withNamespaces } from 'react-i18next'
+import { Redux } from 'redux-render'
+import { Link, Redirect } from '@reach/router'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import { withStyles } from '@material-ui/core/styles'
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import BlockProducerRadar from 'components/block-producer-radar'
+import bpParameters from 'config/comparison-parameters'
 
-import ProfilePage from 'components/block-producer-profile-page'
-import store from 'store'
-
-const { dispatch } = store
-
-const style = theme => ({})
-
-class ProfilePageComponent extends Component {
-  componentDidMount () {
-    const { account } = this.props
-    dispatch.blockProducers.getBP(account)
+const style = theme => ({
+  container: {
+    padding: 10
+  },
+  bpName: {
+    marginLeft: 6
+  },
+  accountCircle: {
+    color: theme.palette.secondary.light
+  },
+  box: {
+    padding: 10
+  },
+  title: {
+    color: '#5cf68a',
+    marginBottom: 10
+  },
+  subTitle: {
+    fontWeight: 'bold'
+  },
+  value: {
+    marginLeft: 4
+  },
+  category: {
+    marginTop: 10
   }
+})
 
-  render () {
-    return (
-      <Redux selector={state => ({ blockProducers: state.blockProducers })}>
-        {({ blockProducers }) => (
-          <ProfilePage currentBP={blockProducers.currentBP} />
-        )}
-      </Redux>
-    )
-  }
-}
+const BlockProducerProfile = ({ classes, account, ...props }) => (
+  <Redux
+    selector={({ blockProducers: { list } }) => ({
+      blockProducer: list.find(bp => bp.producer_account_name === account)
+    })}
+  >
+    {({ blockProducer }) => {
+      if (!blockProducer) {
+        return <Redirect to='/not-found' />
+      }
+      return (
+        <Grid
+          container
+          justify='center'
+          spacing={16}
+          className={classes.container}
+        >
+          <Grid item xs={12}>
+            <Grid container spacing={16} direction='row' alignItems='center'>
+              <Link to='/block-producers'>
+                <IconButton>
+                  <KeyboardArrowLeft />
+                </IconButton>
+              </Link>
+              <Typography variant='caption'>ALL BLOCK PRODUCERS</Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper>
+              <Grid
+                container
+                direction='row'
+                alignItems='center'
+                className={classes.box}
+              >
+                <Grid container direction='row' alignItems='center'>
+                  <Grid item xs={12}>
+                    <Grid container direction='row' alignItems='center'>
+                      <Grid item xs={4}>
+                        <Grid container direction='row' alignItems='center'>
+                          <AccountCircle className={classes.accountCircle} />
+                          <Typography
+                            variant='title'
+                            className={classes.bpName}
+                          >
+                            {blockProducer.producer_account_name || ''}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <Grid item xs={8}>
+                        <Grid container direction='row' justify='flex-end'>
+                          <Button
+                            style={{
+                              marginRight: 10,
+                              backgroundColor: '#010318'
+                            }}
+                          >
+                            ADD TO COMPARE
+                          </Button>
+                          <Button
+                            className='textPrimary'
+                            variant='contained'
+                            size='small'
+                            color='secondary'
+                          >
+                            RATE
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid container direction='row' style={{ marginTop: 10 }}>
+                  <Grid item xs={4}>
+                    <Grid
+                      container
+                      direction='column'
+                      className={classes.category}
+                    >
+                      <Typography variant='subtitle1' className={classes.title}>
+                        General Information
+                      </Typography>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Location:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          Costa Rica
+                        </Typography>
+                      </Grid>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Website:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          eoscostarica.io
+                        </Typography>
+                      </Grid>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Position:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          #15
+                        </Typography>
+                      </Grid>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Votes:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          1.56 %
+                        </Typography>
+                      </Grid>
+                    </Grid>
 
-ProfilePageComponent.propTypes = {
+                    <Grid
+                      container
+                      direction='column'
+                      className={classes.category}
+                    >
+                      <Typography variant='subtitle1' className={classes.title}>
+                        Economy
+                      </Typography>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Balance:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          4.0800 EOS
+                        </Typography>
+                      </Grid>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Staked:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          6.7800 EOS
+                        </Typography>
+                      </Grid>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Daily Reward:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          500 EOS
+                        </Typography>
+                      </Grid>
+                    </Grid>
+
+                    <Grid
+                      container
+                      direction='column'
+                      className={classes.category}
+                    >
+                      <Typography variant='subtitle1' className={classes.title}>
+                        Ratings
+                      </Typography>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Rate:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          8.5
+                        </Typography>
+                      </Grid>
+                      <Grid container direction='row'>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.subTitle}
+                        >
+                          Raters:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          450 accounts
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Grid container direction='column'>
+                      <Grid item xs={12} style={{ backgroundColor: '#000' }}>
+                        <BlockProducerRadar
+                          bpData={{
+                            labels: bpParameters,
+                            datasets: [blockProducer.data]
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.title}
+                        >
+                          From Their Website:
+                        </Typography>
+                        <Typography
+                          variant='subtitle1'
+                          className={classes.value}
+                        >
+                          We believe that technology is only as good as the
+                          people and intentions behind it. We want EOS to be
+                          decentralized, promote freedom, cut, inefficiencies in
+                          government, help sustainable development, secure
+                          censorship-resistance, increase individual sovereignty
+                          and advance citizen advocacy Each of the projects we
+                          promote must provide a “Proof of impact” in the
+                          advancement of democracy
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      )
+    }}
+  </Redux>
+)
+
+BlockProducerProfile.propTypes = {
+  classes: PropTypes.object,
   account: PropTypes.string
 }
 
 export default withStyles(style)(
-  withNamespaces('bpProfilePage')(ProfilePageComponent)
+  withNamespaces('bpProfilePage')(BlockProducerProfile)
 )
