a0d30570db5e9e8208470dc0ce08ff38a10b1849
Gabo Esquivel <contact@gaboesquivel.com>
2019-01-05 22:37:24 -0600

    wip: working on proxy assertion
    Former-commit-id: f6d8a2e8d1dc8a54b158c0b31a39f9f2f5fae223

--- contracts/eoseosrateio/eoseosrateio.cpp
+++ contracts/eoseosrateio/eoseosrateio.cpp
@@ -1,74 +1,79 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/print.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/multi_index.hpp>
 
 using namespace eosio;
-using std::string;
+using namespace std;
 
 CONTRACT eoseosrateio : public contract {
-public:
-  using contract::contract;
+  public:
+    using contract::contract;
 
-  eoseosrateio(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+    ACTION rateproducer(name user, name bp, string ratings_json) {
+      require_auth(user);
 
-  ACTION rateproducer(name rater_account, const string json) {
-    require_auth(rater_account);
+      // user must be a proxy
+      voters_table _voters(_self, "eosio"_n.value);
+      auto pitr = _voters.find( user.value );
+      if ( pitr != _voters.end() ) {
+        eosio_assert( !pitr->is_proxy, "only proxy accounts are allowed to rate at the moment" );
+      }
 
-    // rater_account must be a proxy
-    auto pitr = _voters.find( rater_account.value );
-    if ( pitr != _voters.end() ) {
-      eosio_assert( !pitr->is_proxy, "only proxy accounts are allowed to rate at the moment" );
+      //TODO: bp must be a registered block producer
+
+      // the payload must be ratings_json.
+      eosio_assert(ratings_json[0] == '{', "payload must be ratings_json");
+      eosio_assert(ratings_json[ratings_json.size()-1] == '}', "payload must be ratings_json");
+
+      // upsert bp rating
+      rate_index _ratings(_code, _code.value);
+      auto key = (static_cast<uint128_t>(user.value) << 64) | bp.value;
+      auto iterator = _ratings.find(key);
+      if( iterator == _ratings.end() ) {
+        _ratings.emplace(user, [&]( auto& row ) {
+          row.key = key;
+          row.user = user;
+          row.bp = bp;
+          row.created_at = current_time();
+          row.ratings_json = ratings_json;
+        });
+      } else {
+        _ratings.modify(iterator, user, [&]( auto& row ) {
+          row.user = user;
+          row.bp = bp;
+          row.created_at = current_time();
+          row.ratings_json = ratings_json;
+        });
+      }
     }
 
-    // the payload must be json.
-    eosio_assert(json[0] == '{', "payload must be json");
-    eosio_assert(json[json.size()-1] == '}', "payload must be json");
-    rate_index ratings(_code, _code.value);
+  private:
+    TABLE rate {
+      uint128_t key;
+      name user;
+      name bp;
+      string ratings_json;
+      uint64_t created_at;
 
-    auto iterator = ratings.find(rater_account.value);
-    if( iterator == ratings.end() )
-    {
-      ratings.emplace(rater_account, [&]( auto& row ) {
-          row.key = rater_account;
-          row.json = json;
-      });
-    }
-    else {
-      ratings.modify(iterator, rater_account, [&]( auto& row ) {
-        row.json = json;
-      });
-    }
-  }
+      uint64_t primary_key() const { return key; }
+      uint64_t by_user() const { return user.value; }
+      uint64_t by_bp() const { return bp.value; }
+    };
 
-private:
-  TABLE rate {
-      name key;
-      string json;
-      uint64_t primary_key() const { return key.value; }
-  };
+    typedef eosio::multi_index<"ratings"_n, rate,
+        indexed_by<"user"_n, const_mem_fun<rate, uint64_t, &rate::by_user>>,
+        indexed_by<"bp"_n, const_mem_fun<rate, uint64_t, &rate::by_bp>>
+      > rate_index;
 
-  typedef eosio::multi_index<"ratings"_n, rate> rate_index;
-
-  TABLE voter_info {
+    TABLE voter_info {
       name                owner;     /// the voter
       name                proxy;     /// the proxy set by the voter, if any
       std::vector<name>   producers; /// the producers approved by this voter if no proxy set
       int64_t             staked = 0;
-
-      /**
-       *  Every time a vote is cast we must first "undo" the last vote weight, before casting the
-       *  new vote weight.  Vote weight is calculated as:
-       *
-       *  stated.amount * 2 ^ ( weeks_since_launch/weeks_per_year)
-       */
       double              last_vote_weight = 0; /// the vote weight cast the last time the vote was updated
-
-      /**
-       * Total vote weight delegated to this voter.
-       */
       double              proxied_vote_weight= 0; /// the total vote weight delegated to this voter as a proxy
       bool                is_proxy = 0; /// whether the voter is a proxy for others
-
-
       uint32_t            reserved1 = 0;
       uint32_t            reserved2 = 0;
       eosio::asset        reserved3;
@@ -77,9 +82,9 @@ private:
 
       // explicit serialization macro is not necessary, used here only to improve compilation time
       EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(staked)(last_vote_weight)(proxied_vote_weight)(is_proxy)(reserved1)(reserved2)(reserved3) )
-   };
+    };
 
-   typedef eosio::multi_index<N(eosio), voter_info > voters_table;
+    typedef eosio::multi_index<"voters"_n, eoseosrateio::voter_info > voters_table;
 };
 
 EOSIO_DISPATCH(eoseosrateio, (rateproducer));
