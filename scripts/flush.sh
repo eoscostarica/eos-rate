990648e342668e05442e6606a313a259017b5000
Gabo Esquivel <contact@gaboesquivel.com>
2019-01-15 21:49:05 -0600

    feat(postgres): use hasura migrations

--- scripts/flush.sh
+++ scripts/flush.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Flushing all blockchain and databases data"
-docker-compose down -v && rm -rf ./services/postgres/.dbdata
+echo "Flushing all docker containers and volumes"
+docker-compose down -v
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,13 @@ docker-compose up -d
 
 # wait 10s for the docker services to start
 sleep 10s
+
+# run hasura postgres migrations
+source $(dirname $0)/migrate.sh
+
+# start the reactjs client
 cd services/frontend
-yarn install
+yarn
 yarn start
 
 cd ../..
