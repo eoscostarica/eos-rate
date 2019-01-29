f35b3bb9a03bd84fb5f3e742985c3c2d2b613665
Daniel Prado <danazkari@gmail.com>
2019-01-29 07:57:30 -0600

    fix(db): add proper hasura migrations
    Former-commit-id: 783c658eaf392a2b82ae502c4dec6d0637c587b1

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,9 +5,6 @@ docker-compose up -d --build postgres demux hasura
 # wait 10s for the docker services to start
 sleep 10s
 
-# run hasura postgres migrations
-source $(dirname $0)/migrate.sh
-
 # start the reactjs client
 cd services/frontend
 yarn
