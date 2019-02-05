a1b70c3ba4e48a54f2e850c5e1639ea9c9803974
Gabo Esquivel <contact@gaboesquivel.com>
2019-02-05 11:46:31 -0600

    fix(eslint): add standard to demux service

--- services/demux/src/services/update-bps/index.js
+++ services/demux/src/services/update-bps/index.js
@@ -1,32 +1,33 @@
-const cron = require("node-cron");
-const EosApi = require("eosjs-api");
-const humanToCron = require("human-to-cron");
-const massive = require("massive");
-const get = require("lodash.get");
+const cron = require('node-cron')
+const EosApi = require('eosjs-api')
+const humanToCron = require('human-to-cron')
+const massive = require('massive')
+const get = require('lodash.get')
+const fetch = require('node-fetch')
 
 const dbConfig = {
-  user: process.env.DB_USER || "user",
-  password: process.env.DB_PASSWORD || "pass",
-  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || 'user',
+  password: process.env.DB_PASSWORD || 'pass',
+  host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || "eosrate",
-  schema: process.env.DB_SCHEMA || "public"
-};
+  database: process.env.DB_NAME || 'eosrate',
+  schema: process.env.DB_SCHEMA || 'public'
+}
 
 // gets data from mainnet
 const getBlockProducersData = async () => {
   const eos = EosApi({
     httpEndpoint: process.env.EOS_API_ENDPOINT,
     verbose: false
-  });
-  const { rows: producers } = await eos.getProducers({ json: true, limit: 1000 });
+  })
+  const { rows: producers } = await eos.getProducers({ json: true, limit: 1000 })
   const { rows: bpJsons } = await eos.getTableRows({
     json: true,
-    code: "producerjson",
-    scope: "producerjson",
-    table: "producerjson",
+    code: 'producerjson',
+    scope: 'producerjson',
+    table: 'producerjson',
     limit: 1000
-  });
+  })
 
   const allProducers = producers.reduce(
     (result, producer) => [
@@ -34,88 +35,87 @@ const getBlockProducersData = async () => {
       {
         owner: producer.owner,
         system: { ...producer },
-        bpJson: get(bpJsons.find(bpJson => bpJson.owner === producer.owner), "json", {})
+        bpJson: get(bpJsons.find(bpJson => bpJson.owner === producer.owner), 'json', {})
       }
     ],
     []
-  );
+  )
 
   const requests = allProducers
     .filter(({ bpJson, system }) => !Object.keys(bpJson) && system.url)
     .map(({ system: { url } }) => {
-      let result = url;
-      if (!url.startsWith("http")) {
-        result = `http://${url}`;
+      let result = url
+      if (!url.startsWith('http')) {
+        result = `http://${url}`
       }
-      if (!url.endsWith(".json")) {
-        result = `${result}/bp.json`;
+      if (!url.endsWith('.json')) {
+        result = `${result}/bp.json`
       }
-      return result;
+      return result
     })
     .map(url =>
       fetch(url)
         .then(res => res.json())
         .catch(e => {
-          console.error(e);
+          console.error(e)
         })
-    );
+    )
 
-  const allJsons = await Promise.all(requests);
+  const allJsons = await Promise.all(requests)
   const result = allProducers.map(producer => ({
     ...producer,
     bpJson: Object.keys(producer.bpJson).length
       ? producer.bpJson
       : allJsons.find(bpJson => bpJson && bpJson.producer_account_name === producer.owner) || {}
-  }));
+  }))
 
-  return result;
-};
+  return result
+}
 
 // updates the postgresdb
 const updateBlockProducersData = async () => {
-  console.log("==== updating block producer info ====");
-  const db = await massive(dbConfig);
-  const producersData = await getBlockProducersData();
+  console.log('==== updating block producer info ====')
+  const db = await massive(dbConfig)
+  const producersData = await getBlockProducersData()
 
   const saveBP = async ({ owner, system, bpJson: bpjson }) => {
-    console.log(`try saving ${owner}`);
+    console.log(`try saving ${owner}`)
     const bpData = {
       owner,
       system,
       bpjson
-    };
+    }
 
     try {
-      const result = await db.producers.save(bpData);
+      const result = await db.producers.save(bpData)
       if (!result) {
-        const insertResult = await db.producers.insert(bpData);
+        const insertResult = await db.producers.insert(bpData)
         if (!insertResult) {
-          console.log(`couldnt save or insert ${owner}`);
-          return;
+          console.log(`couldnt save or insert ${owner}`)
+          return
         }
       }
-      console.log(`succefully saved ${owner}`);
+      console.log(`succefully saved ${owner}`)
     } catch (error) {
-      console.error(error);
-      return;
+      console.error(error)
     }
-  };
+  }
 
   for (let bp of producersData) {
-    await saveBP(bp);
+    await saveBP(bp)
   }
 
   // TODO : better error handling, report and retry unfulffilled
-};
+}
 
 const run = async () => {
   try {
     // schedule the cron job
-    cron.schedule(humanToCron("once each day"), updateBlockProducersData);
-    updateBlockProducersData();
+    cron.schedule(humanToCron('once each day'), updateBlockProducersData)
+    updateBlockProducersData()
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
-run();
+run()
