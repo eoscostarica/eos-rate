66b06966324a4d881f83f606e112f35108823489
Daniel Prado <danazkari@gmail.com>
2019-02-09 11:12:02 -0600

    fix(web): remove redux-render from rate page

--- services/frontend/src/routes/block-producers/block-producer-rate.js
+++ services/frontend/src/routes/block-producers/block-producer-rate.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withNamespaces } from 'react-i18next'
-import { Redux } from 'redux-render'
+import { connect } from 'react-redux'
 import { Link, Redirect } from '@reach/router'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
@@ -54,233 +54,233 @@ const style = theme => ({
   }
 })
 
-const BlockProducerRate = ({ classes, account, ...props }) => (
-  <Redux
-    selector={({ blockProducers: { list } }) => ({
-      blockProducer: list.find(
-        bp => bp.bpjson.producer_account_name === account
-      )
-    })}
-  >
-    {({ blockProducer }) => {
-      if (!blockProducer) {
-        return <Redirect to='/not-found' />
-      }
-      return (
+const BlockProducerRate = ({ classes, account, list, ...props }) => {
+  const blockProducer = list.find(
+    bp => bp.bpjson.producer_account_name === account
+  )
+  if (!blockProducer) {
+    return <Redirect to='/not-found' />
+  }
+  return (
+    <Grid container justify='center' spacing={16} className={classes.container}>
+      <Grid item xs={12}>
         <Grid
           container
-          justify='center'
           spacing={16}
-          className={classes.container}
+          direction='row'
+          alignItems='center'
+          className={classes.breadcrumbText}
         >
-          <Grid item xs={12}>
+          <Link to='/block-producers'>
+            <IconButton>
+              <KeyboardArrowLeft />
+            </IconButton>
+          </Link>
+          <Typography variant='caption'>All Block Producers</Typography>
+          <Link
+            to={`/block-producers/${
+              blockProducer.bpjson.producer_account_name
+            }`}
+          >
+            <IconButton>
+              <KeyboardArrowLeft />
+            </IconButton>
+          </Link>
+          <Typography variant='caption'>
+            {blockProducer.bpjson.producer_account_name || ''}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper>
+          <Grid
+            container
+            direction='row'
+            alignItems='center'
+            className={classes.box}
+          >
+            <Grid container direction='row' alignItems='center'>
+              <Grid item xs={12}>
+                <Grid container direction='row' alignItems='center'>
+                  <AccountCircle className={classes.accountCircle} />
+                  <Typography variant='h6' className={classes.bpName}>
+                    {blockProducer.bpjson.producer_account_name || ''}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
             <Grid
               container
-              spacing={16}
               direction='row'
-              alignItems='center'
-              className={classes.breadcrumbText}
+              spacing={16}
+              style={{ marginTop: 10 }}
             >
-              <Link to='/block-producers'>
-                <IconButton>
-                  <KeyboardArrowLeft />
-                </IconButton>
-              </Link>
-              <Typography variant='caption'>All Block Producers</Typography>
-              <Link
-                to={`/block-producers/${
-                  blockProducer.bpjson.producer_account_name
-                }`}
-              >
-                <IconButton>
-                  <KeyboardArrowLeft />
-                </IconButton>
-              </Link>
-              <Typography variant='caption'>
-                {blockProducer.bpjson.producer_account_name || ''}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper>
-              <Grid
-                container
-                direction='row'
-                alignItems='center'
-                className={classes.box}
-              >
-                <Grid container direction='row' alignItems='center'>
+              <Grid item xs={12} sm={5}>
+                <Typography variant='subtitle1' className={classes.title}>
+                  Rate Block Producer
+                </Typography>
+                <Typography paragraph>
+                  Use the sliders to rate the BP.
+                </Typography>
+                <Typography paragraph>
+                  If you feel that you do not have enough knowledge about a
+                  specific category you can disable it.
+                </Typography>
+                <Typography paragraph>
+                  Publish the rate by signing with Scatter.
+                </Typography>
+                {/* TODO: Iterate over bpParameters */}
+                <Grid container style={{ marginTop: 30 }}>
                   <Grid item xs={12}>
-                    <Grid container direction='row' alignItems='center'>
-                      <AccountCircle className={classes.accountCircle} />
-                      <Typography variant='h6' className={classes.bpName}>
-                        {blockProducer.bpjson.producer_account_name || ''}
-                      </Typography>
-                    </Grid>
+                    <Typography paragraph style={{ margin: 0 }}>
+                      Transparency{' '}
+                      <Tooltip title='Lorem ipsum' placement='right'>
+                        <HelpOutline
+                          fontSize='inherit'
+                          className={classes.topicIcon}
+                        />
+                      </Tooltip>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <Slider min={0} max={5} step={1} />
+                      <Switch checked />
+                    </div>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography paragraph style={{ margin: 0 }}>
+                      Infrastruture{' '}
+                      <Tooltip title='Lorem ipsum' placement='right'>
+                        <HelpOutline
+                          fontSize='inherit'
+                          className={classes.topicIcon}
+                        />
+                      </Tooltip>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <Slider min={0} max={5} step={1} />
+                      <Switch checked />
+                    </div>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography paragraph style={{ margin: 0 }}>
+                      Trustiness{' '}
+                      <Tooltip title='Lorem ipsum' placement='right'>
+                        <HelpOutline
+                          fontSize='inherit'
+                          className={classes.topicIcon}
+                        />
+                      </Tooltip>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <Slider min={0} max={5} step={1} />
+                      <Switch checked />
+                    </div>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography paragraph style={{ margin: 0 }}>
+                      Community{' '}
+                      <Tooltip title='Lorem ipsum' placement='right'>
+                        <HelpOutline
+                          fontSize='inherit'
+                          className={classes.topicIcon}
+                        />
+                      </Tooltip>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <Slider min={0} max={5} step={1} />
+                      <Switch checked />
+                    </div>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography paragraph style={{ margin: 0 }}>
+                      Development{' '}
+                      <Tooltip title='Lorem ipsum' placement='right'>
+                        <HelpOutline
+                          fontSize='inherit'
+                          className={classes.topicIcon}
+                        />
+                      </Tooltip>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <Slider min={0} max={5} step={1} />
+                      <Switch checked />
+                    </div>
                   </Grid>
                 </Grid>
-                <Grid
-                  container
-                  direction='row'
-                  spacing={16}
-                  style={{ marginTop: 10 }}
-                >
-                  <Grid item xs={12} sm={5}>
-                    <Typography variant='subtitle1' className={classes.title}>
-                      Rate Block Producer
-                    </Typography>
-                    <Typography paragraph>
-                      Use the sliders to rate the BP.
-                    </Typography>
-                    <Typography paragraph>
-                      If you feel that you do not have enough knowledge about a
-                      specific category you can disable it.
-                    </Typography>
-                    <Typography paragraph>
-                      Publish the rate by signing with Scatter.
-                    </Typography>
-                    {/* TODO: Iterate over bpParameters */}
-                    <Grid container style={{ marginTop: 30 }}>
-                      <Grid item xs={12}>
-                        <Typography paragraph style={{ margin: 0 }}>
-                          Transparency{' '}
-                          <Tooltip title='Lorem ipsum' placement='right'>
-                            <HelpOutline
-                              fontSize='inherit'
-                              className={classes.topicIcon}
-                            />
-                          </Tooltip>
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                          <Slider min={0} max={5} step={1} />
-                          <Switch checked />
-                        </div>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Typography paragraph style={{ margin: 0 }}>
-                          Infrastruture{' '}
-                          <Tooltip title='Lorem ipsum' placement='right'>
-                            <HelpOutline
-                              fontSize='inherit'
-                              className={classes.topicIcon}
-                            />
-                          </Tooltip>
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                          <Slider min={0} max={5} step={1} />
-                          <Switch checked />
-                        </div>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Typography paragraph style={{ margin: 0 }}>
-                          Trustiness{' '}
-                          <Tooltip title='Lorem ipsum' placement='right'>
-                            <HelpOutline
-                              fontSize='inherit'
-                              className={classes.topicIcon}
-                            />
-                          </Tooltip>
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                          <Slider min={0} max={5} step={1} />
-                          <Switch checked />
-                        </div>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Typography paragraph style={{ margin: 0 }}>
-                          Community{' '}
-                          <Tooltip title='Lorem ipsum' placement='right'>
-                            <HelpOutline
-                              fontSize='inherit'
-                              className={classes.topicIcon}
-                            />
-                          </Tooltip>
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                          <Slider min={0} max={5} step={1} />
-                          <Switch checked />
-                        </div>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Typography paragraph style={{ margin: 0 }}>
-                          Development{' '}
-                          <Tooltip title='Lorem ipsum' placement='right'>
-                            <HelpOutline
-                              fontSize='inherit'
-                              className={classes.topicIcon}
-                            />
-                          </Tooltip>
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                          <Slider min={0} max={5} step={1} />
-                          <Switch checked />
-                        </div>
-                      </Grid>
-                    </Grid>
+              </Grid>
+              <Grid item xs={12} sm={7}>
+                <Grid container direction='column'>
+                  <Grid item xs={12} style={{ backgroundColor: '#000' }}>
+                    <BlockProducerRadar
+                      bpData={{
+                        labels: bpParameters,
+                        datasets: [blockProducer.data]
+                      }}
+                    />
                   </Grid>
-                  <Grid item xs={12} sm={7}>
-                    <Grid container direction='column'>
-                      <Grid item xs={12} style={{ backgroundColor: '#000' }}>
-                        <BlockProducerRadar
-                          bpData={{
-                            labels: bpParameters,
-                            datasets: [blockProducer.data]
-                          }}
-                        />
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Grid
-                          container
-                          justify='flex-end'
-                          style={{ marginTop: 10 }}
-                        >
-                          <Button
-                            className='textPrimary'
-                            variant='contained'
-                            size='small'
-                            color='secondary'
-                            style={{ marginRight: 10 }}
-                          >
-                            Publish Rating
-                          </Button>
-                          <Button
-                            href={`/block-producers/${
-                              blockProducer.bpjson.producer_account_name
-                            }`}
-                            variant='contained'
-                            size='small'
-                          >
-                            Cancel
-                          </Button>
-                        </Grid>
-                      </Grid>
+                  <Grid item xs={12}>
+                    <Grid
+                      container
+                      justify='flex-end'
+                      style={{ marginTop: 10 }}
+                    >
+                      <Button
+                        className='textPrimary'
+                        variant='contained'
+                        size='small'
+                        color='secondary'
+                        style={{ marginRight: 10 }}
+                      >
+                        Publish Rating
+                      </Button>
+                      <Button
+                        href={`/block-producers/${
+                          blockProducer.bpjson.producer_account_name
+                        }`}
+                        variant='contained'
+                        size='small'
+                      >
+                        Cancel
+                      </Button>
                     </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Paper>
+            </Grid>
           </Grid>
-        </Grid>
-      )
-    }}
-  </Redux>
-)
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+}
 
 BlockProducerRate.propTypes = {
   classes: PropTypes.object,
-  account: PropTypes.string
+  account: PropTypes.string,
+  list: PropTypes.array
 }
 
+const mapStateToProps = ({ blockProducers: { list } }) => ({
+  list
+})
+
+const mapDispatchToProps = () => ({})
+
 export default withStyles(style)(
-  withNamespaces('bpProfilePage')(BlockProducerRate)
+  withNamespaces('bpProfilePage')(
+    connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )(BlockProducerRate)
+  )
 )
