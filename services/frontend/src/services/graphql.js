9dbd7bcc62a4bcc8d3d5f5c4ea5734c0d0d470b8
Gabo Esquivel <contact@gaboesquivel.com>
2019-02-09 13:18:58 -0600

    feat(ci): configs for staging env

--- services/frontend/src/services/graphql.js
+++ services/frontend/src/services/graphql.js
@@ -1,7 +1,8 @@
 import ApolloClient from 'apollo-boost'
 
+console.log(process.env)
 const client = new ApolloClient({
-  uri: process.env.GRAPHQL_URL || 'http://localhost:8088/v1alpha1/graphql'
+  uri: process.env.REACT_APP_GRAPHQL_URL || 'http://localhost:8088/v1alpha1/graphql'
 })
 
 export default client
