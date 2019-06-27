fa882f8ed5ed3b8e1cb576b6c8d84732b280c11b
Arturo Campos <arturo025@gmail.com>
2019-06-27 00:36:17 -0600

    refactor(frontend): use secure node
    Former-commit-id: 6c0951e57c066a38f917654047c40f42260cc10f

--- services/frontend/src/services/wallet.js
+++ services/frontend/src/services/wallet.js
@@ -30,8 +30,8 @@ export const getWallet = async walletIndex => {
     appName: 'EOSRate',
     network: {
       host: 'jungle2.cryptolions.io',
-      port: 80,
-      protocol: 'http',
+      port: 443,
+      protocol: 'https',
       chainId:
         'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473'
     },
