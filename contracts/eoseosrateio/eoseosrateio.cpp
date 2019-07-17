cf602ae971a4432742c696ff06f5a8cce9ba9134
kriskoin <cristian@eoscostarica.io>
2019-07-16 18:04:41 -0600

    wip: is_proxy function added

--- contracts/eoseosrateio/eoseosrateio.cpp
+++ contracts/eoseosrateio/eoseosrateio.cpp
@@ -6,25 +6,53 @@
 using namespace eosio;
 using namespace std;
 
+namespace eosio {
+
+   constexpr name system_account{"eosio"_n};
+
+   struct voter_info {
+      name                owner;
+      name                proxy;
+      std::vector<name>   producers;
+      int64_t             staked = 0;
+      double              last_vote_weight = 0;
+      double              proxied_vote_weight= 0;
+      bool                is_proxy = 0;
+      uint32_t            flags1 = 0;
+      uint32_t            reserved2 = 0;
+      eosio::asset        reserved3;
+
+      uint64_t primary_key()const { return owner.value; }
+
+      EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(staked)(last_vote_weight)(proxied_vote_weight)(is_proxy)(flags1)(reserved2)(reserved3) )
+   };
+
+   typedef eosio::multi_index< "voters"_n, voter_info >  voters_table;
+
+   bool is_proxy(name name) {
+      voters_table _voters(system_account, system_account.value);
+
+      auto it = _voters.find(name.value);
+      return it != _voters.end() && it->is_proxy;
+   }
+
+} /// namespace eosio
+
 CONTRACT eoseosrateio : public contract {
   public:
     using contract::contract;
 
     ACTION rateproducer(name user, name bp, string ratings_json) {
-      require_auth(user);
+      require_auth(_self);
 
       // user must be a proxy
-      voters_table _voters(_self, "eosio"_n.value);
-      auto pitr = _voters.find( user.value );
-      if ( pitr != _voters.end() ) {
-        eosio_assert( !pitr->is_proxy, "only proxy accounts are allowed to rate at the moment" );
-      }
-
+      check( is_proxy(user), "only proxy accounts are allowed to rate at the moment" );
+      
       //TODO: bp must be a registered block producer
 
       // the payload must be ratings_json.
-      eosio_assert(ratings_json[0] == '{', "payload must be ratings_json");
-      eosio_assert(ratings_json[ratings_json.size()-1] == '}', "payload must be ratings_json");
+      check(ratings_json[0] == '{', "payload must be ratings_json");
+      check(ratings_json[ratings_json.size()-1] == '}', "payload must be ratings_json");
 
       // upsert bp rating
       producers_table bps(_self, _self.value);
@@ -34,7 +62,7 @@ CONTRACT eoseosrateio : public contract {
       auto existing_rating = uniq_rating_index.find(uniq_rating);
 
       if( existing_rating == uniq_rating_index.end() ) {
-          bps.emplace(user, [&]( auto& row ) {
+          bps.emplace(_self, [&]( auto& row ) {
             row.id = bps.available_primary_key();
             row.uniq_rating = uniq_rating;
             row.user = user;
@@ -45,7 +73,7 @@ CONTRACT eoseosrateio : public contract {
           });
 
       } else {
-         uniq_rating_index.modify(existing_rating, user, [&]( auto& row ) {
+         uniq_rating_index.modify(existing_rating, _self, [&]( auto& row ) {
            row.user = user;
            row.bp = bp;
            row.created_at = current_time();
@@ -88,30 +116,6 @@ CONTRACT eoseosrateio : public contract {
         indexed_by<"bp"_n, const_mem_fun<block_producer, uint64_t, &block_producer::by_bp>>
       > producers_table;
 
-    TABLE voter_info {
-      name                owner;     /// the voter
-      name                proxy;     /// the proxy set by the voter, if any
-      std::vector<name>   producers; /// the producers approved by this voter if no proxy set
-      int64_t             staked = 0;
-      double              last_vote_weight = 0; /// the vote weight cast the last time the vote was updated
-      double              proxied_vote_weight= 0; /// the total vote weight delegated to this voter as a proxy
-      bool                is_proxy = 0; /// whether the voter is a proxy for others
-      uint32_t            reserved1 = 0;
-      uint32_t            reserved2 = 0;
-      eosio::asset        reserved3;
-
-      uint64_t primary_key()const { return owner.value; }
-
-      // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(staked)(last_vote_weight)(proxied_vote_weight)(is_proxy)(reserved1)(reserved2)(reserved3) )
-    };
-
-    typedef eosio::multi_index<"voters"_n, eoseosrateio::voter_info > voters_table;
 };
 
 EOSIO_DISPATCH(eoseosrateio, (rateproducer));
-
-
-// NOTE:
-// the table name ratings is damaged on the jungle testnet, changes in the struct without erasing the data on RAM seem have caused the problem.
-// you need to be carefull when changing table structure, you cant do this if table is with data, you need to do new table with new structure and migrate data.
