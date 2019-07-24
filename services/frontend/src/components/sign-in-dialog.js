1d192b7d58e83b335099f6f350566dca9fa31660
Arturo Campos <arturo025@gmail.com>
2019-07-24 01:50:53 -0600

    feat(frontend): move sign in options to a dropdown menu
    Former-commit-id: 2eb287dcb2758d661158b8fef9d5edb775c1a770

--- services/frontend/src/components/sign-in-dialog.js
+++ services/frontend/src/components/sign-in-dialog.js
@@ -1,83 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import { withNamespaces } from 'react-i18next'
-// import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Dialog from '@material-ui/core/Dialog'
 import LinearProgress from '@material-ui/core/LinearProgress'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-// import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemText from '@material-ui/core/ListItemText'
 import red from '@material-ui/core/colors/red'
-import Typography from '@material-ui/core/Typography'
-
-import walletProviders from 'config/wallet-providers'
-import { useWalletDispatch, useWalletState } from 'hooks/wallet'
 
 const styles = {
   errorMsg: {
-    color: red[500],
-    padding: 10,
-    textAlign: 'center'
-  },
-  walletIcon: {
-    width: '100%'
-  },
-  wrapper: {
-    padding: '10px 30px'
+    color: red[500]
   }
 }
 
-const SignInDialog = ({ classes, onClose, open, t }) => {
-  const { connectWallet } = useWalletDispatch()
-  const walletState = useWalletState()
+const SignInDialog = ({ classes, connecting, error, provider, t }) => {
+  const [errorDialogOpen, setErrorDialogOpen] = useState(Boolean(error))
+  useEffect(() => setErrorDialogOpen(Boolean(error)), [error])
 
   return (
-    <Dialog
-      aria-labelledby='sign-in-dialog-title'
-      onClose={() => onClose()}
-      open={open}
-    >
-      <DialogTitle id='sign-in-dialog-title'>
-        {t('connectToWallet')}
-      </DialogTitle>
-      <List className={classes.wrapper}>
-        {walletProviders.map(provider => (
-          <ListItem
-            button
-            disabled={walletState.connecting}
-            key={provider.id}
-            onClick={() => connectWallet(provider.id)}
-          >
-            {/* <ListItemAvatar>
-              <Avatar className={classes.avatar}>
-                <img
-                  alt={provider.name}
-                  className={classes.walletIcon}
-                  src={`/images/wallets/${provider.id}.svg`}
-                />
-              </Avatar>
-            </ListItemAvatar> */}
-            <ListItemText primary={provider.name} />
-          </ListItem>
-        ))}
-      </List>
-      {walletState.error && (
-        <Typography className={classes.errorMsg}>
-          Error connecting to wallet.
-        </Typography>
-      )}
-      {walletState.connecting && <LinearProgress />}
-    </Dialog>
+    <>
+      <Dialog aria-labelledby='sign-in-dialog-title' open={Boolean(connecting)}>
+        <DialogTitle id='sign-in-dialog-title'>
+          {provider ? `${t('connectingWith')} ${provider}` : t('connecting')}
+        </DialogTitle>
+        <DialogContent>
+          <LinearProgress />
+        </DialogContent>
+      </Dialog>
+      <Dialog aria-labelledby='error-dialog-title' open={errorDialogOpen}>
+        <DialogTitle id='sign-in-dialog-title'>
+          {t('connectingErrorTitle')}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText className={classes.errorMsg}>
+            {error}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setErrorDialogOpen(false)}>
+            {t('connectingErrorActionBtn')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
 SignInDialog.propTypes = {
   classes: PropTypes.object.isRequired,
-  onClose: PropTypes.func,
-  open: PropTypes.bool.isRequired,
+  connecting: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+  provider: PropTypes.string,
   t: PropTypes.func.isRequired
 }
 
