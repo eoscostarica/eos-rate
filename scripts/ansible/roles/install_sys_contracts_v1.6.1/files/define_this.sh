9d0b7690ff559d8908e00ac9a95ed26dbfa10543
Cristian Castro <kriskoin@localhost.localdomain>
2019-08-04 23:52:33 -0600

    wip: more versions on cdt and syscontracts
    Former-commit-id: e058bd9eb1010e66e8df01a9d5617ad516eb3981

---
+++ scripts/ansible/roles/install_sys_contracts_v1.6.1/files/define_this.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# set -o errexit
+export CMAKE_PREFIX_PATH="/usr/opt/eosio.cdt/1.6.0/lib/cmake/eosio.cdt/:/root/eos/CMakeModules/"
+export CMAKE_MODULE_PATH="/usr/opt/eosio.cdt/1.6.0/lib/cmake/eosio.cdt/:/root/eos/CMakeModules/"
---
+++ scripts/ansible/roles/publish_eosio_sys_contracts_1.6.1/files/publish_system_contracts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# set -o errexit
+export EOSIO_SYSTEM_CONTRACTS_PUB_KEY="EOS5Rc8VJCyNCYPdE4gQhqPMTetFXkJPagkq4dicPCPGTAr5d4qu8"
+export WALLET_KEY_PATH="/eos_wallet_key/key.txt"
+export CONTRACTS_PATH="/contracts/v1.6.1/build/contracts"
+cleos wallet unlock --password  $(cat ${WALLET_KEY_PATH})
+sleep 3
+cleos set contract eosio.token ${CONTRACTS_PATH}/eosio.token
+sleep 5
+cleos set contract eosio.msig ${CONTRACTS_PATH}/eosio.msig
+sleep 10
+cleos set contract eosio ${CONTRACTS_PATH}/eosio.system
+sleep 15
+cleos set contract eosio ${CONTRACTS_PATH}/eosio.bios
+sleep 15
+cleos set contract eosio ${CONTRACTS_PATH}/eosio.wrap
+sleep 15
+cleos push action eosio setpriv '["eosio.msig", 1]' -p eosio@active
+  
