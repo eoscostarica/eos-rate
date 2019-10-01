b8bec8727d10459d5ed34fc4bcfdda96bbeb7755
Kevin Castillo <kecoco16@gmail.com>
2019-09-30 22:53:10 -0600

    feat(frontend): producers_list subscription
    Former-commit-id: d166c8e136748e6b021cb0aa13c4177592d7aa4a

--- services/frontend/src/services/bps.js
+++ services/frontend/src/services/bps.js
@@ -1,43 +1,59 @@
 import mockedBPs from 'mock/bps'
 import client from 'services/graphql'
 import gql from 'graphql-tag'
+import getBPRadarData from 'utils/getBPRadarData'
 
 const getRandomParameter = (max = 10) => Math.floor(Math.random() * max)
 
-export const getAllBPs = async ({ nameFilter = '' } = {}) => {
-  const {
-    data: { producers_list: producers }
-  } = await client.query({
-    query: gql`
-      query blockProducers($nameFilter: String!) {
-        producers_list(
-          where: { _or: { candidate_name: { _ilike: $nameFilter } } }
-          order_by: { total_votes: desc }
-        ) {
-          owner
-          system
-          bpjson
+export const getAllBPs = ({ nameFilter = '', setBPs = () => {} } = {}) =>
+  client
+    .subscribe({
+      query: gql`
+        subscription blockProducers($nameFilter: String!) {
+          producers_list(
+            where: { _or: { candidate_name: { _ilike: $nameFilter } } }
+            order_by: { total_votes: desc }
+          ) {
+            owner
+            system
+            bpjson
+          }
         }
+      `,
+      variables: {
+        nameFilter: `%${nameFilter}%`
       }
-    `,
-    variables: {
-      nameFilter: `%${nameFilter}%`
-    }
-  })
-  return producers.map(producer => ({
-    ...producer,
-    system: {
-      ...producer.system,
-      parameters: {
-        infrastructure: getRandomParameter(),
-        tooling: getRandomParameter(),
-        community: getRandomParameter(),
-        transparency: getRandomParameter(),
-        testnets: getRandomParameter()
+    })
+    .subscribe({
+      next ({ data: { producers_list: producers } }) {
+        const BPs = producers.map(producer => {
+          const parameters = {
+            infrastructure: getRandomParameter(),
+            tooling: getRandomParameter(),
+            community: getRandomParameter(),
+            transparency: getRandomParameter(),
+            testnets: getRandomParameter()
+          }
+
+          return {
+            ...producer,
+            system: {
+              ...producer.system,
+              parameters
+            },
+            data: getBPRadarData({
+              name: producer.owner,
+              parameters
+            })
+          }
+        })
+
+        return setBPs(BPs)
+      },
+      error (err) {
+        console.error('err', err)
       }
-    }
-  }))
-}
+    })
 
 export const findBPs = async (filter = {}) => mockedBPs
 
