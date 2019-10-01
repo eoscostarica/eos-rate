9c687eeb1c918e0287a0f540967ad241f60b7815
Kevin Castillo <kecoco16@gmail.com>
2019-09-30 22:53:10 -0600

    feat(frontend): producers_list subscription

--- services/frontend/src/services/graphql.js
+++ services/frontend/src/services/graphql.js
@@ -1,8 +1,44 @@
-import ApolloClient from 'apollo-boost'
+import ApolloClient from 'apollo-client'
+import { WebSocketLink } from 'apollo-link-ws'
+import { HttpLink } from 'apollo-link-http'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-console.log(process.env)
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URL || 'http://localhost:8088/v1alpha1/graphql'
+// Create an http link:
+const httpLink = new HttpLink({
+  uri:
+    process.env.REACT_APP_GRAPHQL_HTTP_URL ||
+    'http://localhost:8088/v1alpha1/graphql',
+  credentials: 'same-origin'
 })
 
-export default client
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri:
+    process.env.REACT_APP_GRAPHQL_WS_URL ||
+    'ws://localhost:8088/v1alpha1/graphql',
+  options: {
+    reconnect: true
+  }
+})
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
+
+// Instantiate client
+const apolloClient = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
+
+export default apolloClient
