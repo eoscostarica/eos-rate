6bc4cd02d7c07d2845c69249a27f9149081a99dc
Teto Gomez <terencio17@rocketmail.com>
2019-11-04 07:24:58 -0600

    upgrade front page UI (#213)
    Former-commit-id: 5d29938409c8933a49c0df0dfbf2465cafa5437a

--- services/frontend/src/components/icon/index.js
+++ services/frontend/src/components/icon/index.js
@@ -2,37 +2,64 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const src = {
-  menu: require('./menu.svg')
+  menu: require('./menu.svg'),
+  community: require('./community.svg'),
+  infrastructure: require('./infrastructure.svg'),
+  development: require('./development.svg'),
+  transparency: require('./transparency.svg'),
+  trustiness: require('./trustiness.svg')
 }
 
-const Icon = ({ name, size = 16, ...props }) => (
-  <button
-    style={{
-      alignItems: 'center',
-      background: 'none',
-      border: 0,
-      display: 'flex',
-      height: size,
-      justifyContent: 'center',
-      padding: 0,
-      width: size
-    }}
-    {...props}
-  >
-    <img
-      alt={name}
-      src={src[name]}
+const Icon = ({ name, useImage, size, ...props }) => {
+  if (useImage) {
+    return (
+      <img
+        alt={name}
+        src={src[name]}
+        style={{
+          height: size,
+          width: size
+        }}
+        {...props}
+      />
+    )
+  }
+
+  return (
+    <button
       style={{
+        alignItems: 'center',
+        background: 'none',
+        border: 0,
+        display: 'flex',
         height: size,
+        justifyContent: 'center',
+        padding: 0,
         width: size
       }}
-    />
-  </button>
-)
+      {...props}
+    >
+      <img
+        alt={name}
+        src={src[name]}
+        style={{
+          height: size,
+          width: size
+        }}
+      />
+    </button>
+  )
+}
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
-  size: PropTypes.number.isRequired
+  size: PropTypes.number.isRequired,
+  useImage: PropTypes.bool
+}
+
+Icon.defaultProps = {
+  size: 16,
+  useImage: false
 }
 
 export default Icon
