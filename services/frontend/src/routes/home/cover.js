5d29938409c8933a49c0df0dfbf2465cafa5437a
Teto Gomez <terencio17@rocketmail.com>
2019-11-04 07:24:58 -0600

    upgrade front page UI (#213)

--- services/frontend/src/routes/home/cover.js
+++ services/frontend/src/routes/home/cover.js
@@ -19,58 +19,79 @@ const styles = ({ palette, typography }) => ({
     maxWidth: '1024px'
   },
   title: {
-    fontSize: typography.h4.fontSize
+    fontSize: typography.h4.fontSize,
+    marginBottom: 12.5
   },
   paragraph: {
     color: palette.grey[600]
   },
   ctaContainer: {
-    textAlign: 'right'
+    textAlign: 'center'
   },
   chartContainer: {
     maxWidth: '400px',
-    width: '100%'
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  btn: {
+    border: 'solid 0.5px #1eb53a',
+    backgroundColor: 'rgba(99, 183, 107, 0.21)',
+    color: '#63b76b'
+  },
+  leftCoverBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
   }
 })
 
 const HomeCover = ({ classes, t, blockProducer }) => (
   <Grid item container xs={12} className={classes.coverContainer} spacing={24}>
-    <Grid item container xs={12} md={6} justify='center'>
-      <div className={classes.chartContainer}>
-        <BlockProducerRadar
-          bpData={{
-            labels: bpParameters,
-            datasets: [blockProducer.data]
-          }}
-        />
-      </div>
-    </Grid>
-
-    <Grid item xs={12} md={6}>
+    <Grid item xs={12} md={6} className={classes.leftCoverBox}>
       <Typography variant='h5' className={classes.title}>
         {t('cover.title')}
       </Typography>
-
       <Typography
         variant='body2'
         className={classes.paragraph}
         align='justify'
         paragraph
       >
-        {t('cover.paragraph')}
+        {t('cover.paragraph1')}
       </Typography>
-
+      <Typography
+        variant='body2'
+        className={classes.paragraph}
+        align='justify'
+        paragraph
+      >
+        {t('cover.paragraph2')}
+      </Typography>
       <div className={classes.ctaContainer}>
         <Button
-          className='textPrimary'
+          className={classes.btn}
           component={bpLink}
           variant='contained'
-          size='small'
+          size='medium'
           color='secondary'
           to='/block-producers'
+          fullWidth
         >
           {t('cover.cta')}
         </Button>
+      </div>
+    </Grid>
+
+    <Grid item container xs={12} md={6} justify='center'>
+      <div className={classes.chartContainer}>
+        <BlockProducerRadar
+          height={230}
+          bpData={{
+            labels: bpParameters,
+            datasets: [blockProducer.data]
+          }}
+        />
       </div>
     </Grid>
 
