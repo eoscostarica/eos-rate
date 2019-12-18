0d1ac99f24a64ecfe90670efc55cf5039f77a7d9
Kevin Castillo Mora <kecoco16@gmail.com>
2019-12-17 20:01:21 -0600

    feat(frontend): allow users change the current language (#221)

--- services/frontend/src/components/compare-tool/compare-slider-view.js
+++ services/frontend/src/components/compare-tool/compare-slider-view.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
-import { withNamespaces } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 import comparisonParameters from 'config/comparison-parameters'
 import BlockProducerRadar from 'components/block-producer-radar'
@@ -30,31 +30,34 @@ const style = theme => ({
   }
 })
 
-const CompareSliderView = ({ classes, selected, className, t, ...props }) => (
-  <div className={[classes.root, className].join(' ')}>
-    <Typography className={classes.title} variant='h5'>
-      {t('compareToolTitle')}
-    </Typography>
-    <div className={classes.slider}>
-      {selected.map(bp => (
-        <div
-          key={`slider-card-${bp.bpjson.producer_account_name}`}
-          className={classes.sliderCard}
-        >
-          <BlockProducerRadar
-            bpData={{
-              labels: comparisonParameters,
-              datasets: [{ ...bp.data }]
-            }}
-          />
-          <Typography variant='subtitle1' className={classes.bpName}>
-            {bp.bpjson.producer_account_name}
-          </Typography>
-        </div>
-      ))}
+const CompareSliderView = ({ classes, selected, className, ...props }) => {
+  const { t } = useTranslation('translations')
+  return (
+    <div className={[classes.root, className].join(' ')}>
+      <Typography className={classes.title} variant='h5'>
+        {t('compareToolTitle')}
+      </Typography>
+      <div className={classes.slider}>
+        {selected.map(bp => (
+          <div
+            key={`slider-card-${bp.bpjson.producer_account_name}`}
+            className={classes.sliderCard}
+          >
+            <BlockProducerRadar
+              bpData={{
+                labels: comparisonParameters,
+                datasets: [{ ...bp.data }]
+              }}
+            />
+            <Typography variant='subtitle1' className={classes.bpName}>
+              {bp.bpjson.producer_account_name}
+            </Typography>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 CompareSliderView.propTypes = {
   classes: PropTypes.object.isRequired,
@@ -65,6 +68,4 @@ CompareSliderView.propTypes = {
 
 CompareSliderView.defaultProps = {}
 
-export default withStyles(style)(
-  withNamespaces('translations')(CompareSliderView)
-)
+export default withStyles(style)(CompareSliderView)
