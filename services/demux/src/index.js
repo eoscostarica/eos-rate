cf2b16ea5e604f93b82b5afe764df5d5d0bcaf0c
Rubén Abarca Navarro <rubenabix@gmail.com>
2020-01-21 11:56:40 -0600

    Rename contract eoseosrateio --> rateproducer (#253)
    * comment dead code

--- services/demux/src/index.js
+++ services/demux/src/index.js
@@ -1,36 +1,35 @@
-const { BaseActionWatcher } = require('demux')
-const { MongoActionReader } = require('demux-eos')
-const fetch = require('node-fetch')
-const updateBPs = require('./services/update-bps')
-const ObjectActionHandler = require('./ObjectActionHandler')
-const handler = require('./handlerVersions/v1');
+const { BaseActionWatcher } = require("demux");
+const { MongoActionReader } = require("demux-eos");
+const fetch = require("node-fetch");
+const updateBPs = require("./services/update-bps");
+const ObjectActionHandler = require("./ObjectActionHandler");
+const handler = require("./handlerVersions/v1");
 
 (async () => {
-  const headBlockNum = await fetch('https://jungle.eosio.cr/v1/chain/get_info')
-    .then(res => res.json())
-    .then(info => info.head_block_num)
-
-  const actionHandler = new ObjectActionHandler([handler])
-
-  const actionReader = new MongoActionReader(
-    'mongodb://190.10.8.205:27017', // mongoEndpoint: the url of the mongodb instance
-    headBlockNum, // startAtBlock: the first block relevant to our application
-    false, // onlyIrreversible: whether or not to only process irreversible blocks
-    600, // the maximum history length
-    'EOS' // name of the database
-  )
-
-  const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, 500)
-
-  // This must be done before calling watch so the MongoDB connection can be made
-  actionReader.initialize().then(() => {
-    console.log('demux started...')
-    actionWatcher.watch()
-  })
-
+  // const headBlockNum = await fetch('https://jungle.eosio.cr/v1/chain/get_info')
+  //   .then(res => res.json())
+  //   .then(info => info.head_block_num)
+  //
+  // const actionHandler = new ObjectActionHandler([handler])
+  //
+  // const actionReader = new MongoActionReader(
+  //   'mongodb://190.10.8.205:27017', // mongoEndpoint: the url of the mongodb instance
+  //   headBlockNum, // startAtBlock: the first block relevant to our application
+  //   false, // onlyIrreversible: whether or not to only process irreversible blocks
+  //   600, // the maximum history length
+  //   'EOS' // name of the database
+  // )
+  //
+  // const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, 500)
+  //
+  // // This must be done before calling watch so the MongoDB connection can be made
+  // actionReader.initialize().then(() => {
+  //   console.log('demux started...')
+  //   actionWatcher.watch()
+  // })
   /**
    * This function will update the list of block producers
    * and their bp.json file if it exists at all.
    */
-  updateBPs()
-})()
+  updateBPs();
+})();
