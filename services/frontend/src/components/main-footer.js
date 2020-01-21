f88d3c193b7fcf661a9b30a3a91bc76cd490cf89
Teto Gomez <terencio17@rocketmail.com>
2020-01-21 11:37:53 -0600

    refactor(FE): add footer legend (update footer) (#246)
    * refactor(FE): add footer legend (update footer)
    
    * refactor(FE): add style using design

--- services/frontend/src/components/main-footer.js
+++ services/frontend/src/components/main-footer.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { useTranslation } from 'react-i18next'
 import IconButton from '@material-ui/core/IconButton'
 
 import TelegramIcon from 'components/telegram-icon'
@@ -24,37 +26,60 @@ const styles = theme => ({
   link: {
     color: theme.palette.common.white,
     textDecoration: 'none'
+  },
+  legend: {
+    display: 'flex',
+    width: 148,
+    lineHeight: 1.33,
+    fontSize: 8,
+    fontWeight: 500,
+    fontStretch: 'normal',
+    letterSpacing: 1,
+    textAlign: 'right',
+    color: 'rgba(255, 255, 255, 0.6)',
+    paddingRight: 13
   }
 })
 
-const MainFooter = ({ classes, ...props }) => (
-  <AppBar className={classes.root}>
-    <Toolbar>
-      <a
-        href='https://eoscostarica.io'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <img
-          src='/eoscostarica-logo.png'
-          alt='eoscostarica logo'
-          className={classes.eoscostaricaLogo}
-        />
-      </a>
-      <div className={classes.grow} />
-      <IconButton
-        href='https://github.com/eoscostarica'
-        target='_blank'
-        color='inherit'
-      >
-        <GithubIcon />
-      </IconButton>
-      <IconButton href='https://t.me/eoscr' target='_blank' color='inherit'>
-        <TelegramIcon />
-      </IconButton>
-    </Toolbar>
-  </AppBar>
-)
+const MainFooter = ({ classes, ...props }) => {
+  const { t } = useTranslation('footer')
+
+  return (
+    <AppBar className={classes.root}>
+      <Toolbar>
+        <a
+          href='https://eoscostarica.io'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img
+            src='/eoscostarica-logo.png'
+            alt='eoscostarica logo'
+            className={classes.eoscostaricaLogo}
+          />
+        </a>
+        <div className={classes.grow} />
+        <Typography
+          variant='overline'
+          className={classes.legend}
+          align='justify'
+        >
+          {t('legend')}
+        </Typography>
+        <IconButton
+          href='https://github.com/eoscostarica'
+          target='_blank'
+          color='inherit'
+        >
+          <GithubIcon />
+        </IconButton>
+        <IconButton href='https://t.me/eoscr' target='_blank' color='inherit'>
+          <TelegramIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 MainFooter.propTypes = {
   classes: PropTypes.object
