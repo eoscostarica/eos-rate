67fd9b29c9c9747f7ee78a7a88429e3256817a60
Rubén Abarca Navarro <rubenabix@gmail.com>
2020-01-21 11:56:40 -0600

    Rename contract eoseosrateio --> rateproducer (#253)
    * comment dead code

--- services/frontend/src/config/index.js
+++ services/frontend/src/config/index.js
@@ -1,12 +1,10 @@
 const config = {
   appName: 'EOSRate',
-  contract: 'eoseosrateio',
+  contract: 'rateproducer',
   eosApiHost: process.env.REACT_APP_EOS_API_HOST,
   eosApiPort: process.env.REACT_APP_EOS_API_PORT,
   eosApiProtocol: process.env.REACT_APP_EOS_API_PROTOCOL,
-  eosApiUri: `${process.env.REACT_APP_EOS_API_PROTOCOL}://${
-    process.env.REACT_APP_EOS_API_HOST
-  }:${process.env.REACT_APP_EOS_API_PORT}`,
+  eosApiUri: `${process.env.REACT_APP_EOS_API_PROTOCOL}://${process.env.REACT_APP_EOS_API_HOST}:${process.env.REACT_APP_EOS_API_PORT}`,
   eosChainId: process.env.REACT_APP_EOS_CHAIN_ID
 }
 
