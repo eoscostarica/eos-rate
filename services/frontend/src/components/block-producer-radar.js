efff98052e08adfa36016e4515f183000dd58a16
Teto Gomez <terencio17@rocketmail.com>
2020-01-29 08:46:30 -0600

    refactor(FE): refactor UI, remove mock data (#266)
    * refactor(FE): refactor UI, remove mock data
    
    * feat(FE): remove console log

--- services/frontend/src/components/block-producer-radar.js
+++ services/frontend/src/components/block-producer-radar.js
@@ -2,38 +2,45 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Radar } from 'react-chartjs-2'
 
-const BlockProducerRadar = ({ bpData, height, ...props }) => (
-  <Radar
-    height={height}
-    data={() => ({
-      ...bpData
-    })}
-    options={{
-      legend: { display: false },
-      layout: {
-        padding: {
-          bottom: 10
-        }
-      },
-      chartArea: {
-        backgroundColor: '#484656',
-        strokeColor: '#b1afad',
-        lineWidth: 4
-      },
-      scale: {
-        ticks: {
-          display: false,
-          min: 0,
-          max: 10,
-          stepSize: 2
+const BlockProducerRadar = ({ bpData, height, ...props }) => {
+  const bpValidData =
+    bpData.datasets && bpData.datasets.length
+      ? bpData
+      : { ...bpData, datasets: [{ data: [0, 0, 0, 0, 0] }] }
+
+  return (
+    <Radar
+      height={height}
+      data={() => ({
+        ...bpValidData
+      })}
+      options={{
+        legend: { display: false },
+        layout: {
+          padding: {
+            bottom: 10
+          }
         },
-        gridLines: { color: '#6e6b81', lineWidth: 4, circular: true },
-        angleLines: { color: '#6e6b81', lineWidth: 4 },
-        pointLabels: { fontColor: 'white', fontSize: 14 }
-      }
-    }}
-  />
-)
+        chartArea: {
+          backgroundColor: '#484656',
+          strokeColor: '#b1afad',
+          lineWidth: 4
+        },
+        scale: {
+          ticks: {
+            display: false,
+            min: 0,
+            max: 10,
+            stepSize: 2
+          },
+          gridLines: { lineWidth: 4, circular: true },
+          angleLines: { color: '#6e6b81', lineWidth: 4 },
+          pointLabels: { fontColor: 'white', fontSize: 14 }
+        }
+      }}
+    />
+  )
+}
 
 BlockProducerRadar.propTypes = {
   bpData: PropTypes.object,
