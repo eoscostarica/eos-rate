2b5fde5430f112c7a58da99b7571ad0cad88afe1
Jorge Murillo <jorge@eosio.cr>
2020-02-11 17:41:23 -0600

    Light theme initial coloring

--- services/frontend/src/config/theme.js
+++ services/frontend/src/config/theme.js
@@ -7,19 +7,19 @@ const theme = createMuiTheme({
   palette: {
     type: 'dark',
     background: {
-      default: '#5b576f'
+      default: '#ffffff'
     },
     primary: {
-      light: '#2b2e3e',
-      main: '#010318',
-      dark: '#000000',
-      sectionBackground: '#1b1a2b',
-      submenu: '#252838'
+      light: '#787291',
+      main: '#443f56',
+      dark: '#221e38',
+      sectionBackground: '#ffffff',
+      submenu: '#597a81'
     },
     secondary: {
-      light: '#fff',
-      main: '#00c25b',
-      dark: '#00902e',
+      light: '#8ba2a6',
+      main: '#597a81',
+      dark: '#222f32',
       boringGreen: '#63b76b',
       midGreen: '#00902e'
     }
