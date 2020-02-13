99671a594faa67cd42ff1e23869b1443d9778dcd
Xavier Fernandez <xavier@crservers.com>
2020-02-12 20:23:32 -0600

    add first set of BPs

---
+++ services/update-bps/websiteTexts.js
@@ -0,0 +1,307 @@
+const bpGeneralInfo = [
+  {
+    bp: "eoscafeblock",
+    general_info: {
+      websiteText:
+        "EOS Cafe Block is a leading block producer on the EOS blockchain focused on creating value through innovation, technology, and education. Our team is maximizing adoption of the world’s fastest blockchain by adding tangible value to the EOS ecosystem."
+    }
+  },
+  {
+    bp: "costaricaeos",
+    general_info: {
+      websiteText:
+        "We envision a community fostered by democratic values, that promotes an environment of innovation, responsible growth, and prosperity. Our goal is to build a strong EOS ecosystem to promote a culture that encourages learning and sharing of knowledge among people. Also, we are strong believers that technology facilitates collaborating and connecting ideas."
+    }
+  },
+  {
+    bp: "eosvolgabpru",
+    general_info: {
+      websiteText:
+        "The team of EOS Volga consists of highly supportive individuals that do believe in EOS. Each member is striving to make this project great. The company is going to focus on sharing the idea to the public through social media, traditional media, and offline meetups"
+    }
+  },
+  {
+    bp: "alohaeosprod",
+    general_info: {
+      websiteText:
+        "We are committed to the EOSIO blockchain protocol that enables the development of commercial-scale decentralized applications. We promote the community tenets of integrity, transparency, benefit to the community, and the overall success of EOS and EOSIO. Our unique geographic location helps to diversify block production networks as we help bridge the East and West."
+    }
+  },
+  {
+    bp: "eosdotwikibp",
+    general_info: {
+      websiteText:
+        "eosdotwikibp is a independent block producer who dedicate to provide stable and reliable services for EOS community. eosdotwikibp is currently running multiple geo-distributed servers with high-end configuration and high speed internet connection to provide block producing, p2p and API services."
+    }
+  },
+  {
+    bp: "starteosiobp",
+    general_info: {
+      websiteText:
+        "Starteos is a company committed to promoting the blockchain industry and better applications to serve our real economy."
+    }
+  },
+  {
+    bp: "eoshuobipool",
+    general_info: {
+      websiteText:
+        "Huobi Mining Pool is the first mining pool platform that integrates digital currency mining and trading. We are committed to providing users with one-stop services for convenient transactions and efficient returns. The Huobi brand has an excellent reputation and a good reputation in the global blockchain field."
+    }
+  },
+  {
+    bp: "zbeosbp11111",
+    general_info: {
+      websiteText: "Promoting EOS ecosystem and promoting EOS development and value circulation."
+    }
+  },
+  {
+    bp: "atticlabeosb",
+    general_info: {
+      websiteText:
+        "The future decentralized financial system needs arbitrage. That is why we believe in approach that EOS team sticks to. To facilitate the development of EOS ecosystem, Attic Lab as EOS Block Producer strives to share its expertise and resources with the global community."
+    }
+  },
+  {
+    bp: "helloeoscnbp",
+    general_info: {
+      websiteText:
+        "HelloEOS is a strong competitor of the EOS Witness Node. It has comprehensive graphene network development capabilities and maintenance capabilities, and has a community of more than 30,000 people. HelloEOS is committed to building the best EOS community and the best witness node."
+    }
+  },
+  {
+    bp: "eosnewyorkio",
+    general_info: {
+      websiteText:
+        "We are EOS New York, a self-funded and self-owned top 21 Block Producer on the EOS blockchain. Our mission is to always add value back into the EOS platform through enabling community organization, advancing the technology, and providing EOS education materials."
+    }
+  },
+  {
+    bp: "bitfinexeos1",
+    general_info: {
+      websiteText:
+        "The focus of Bitfinex as an EOS Block Producer is on pioneering open-source development of EOSIO technology, sharing openly with other projects in the space to facilitate a stable ecosystem of support, collaboration and innovation. The entirety of our development efforts go towards ensuring the security, scalability and adoption of the EOS ecosystem."
+    }
+  },
+  {
+    bp: "eosiosg11111",
+    general_info: {
+      websiteText:
+        "EOSIO.SG has strong technical research and development capabilities and is a famous technical block producer.eosiosg11111 is Looking forward to your votes"
+    }
+  },
+  {
+    bp: "jedaaaaaaaaa",
+    general_info: {
+      websiteText:
+        "The EOS Developers Association is based in Japan and supports the development of the EOS ecosystem in Japan. We will build an EOS ecosystem, propose online and offline events, gather EOS developers, develop attractive DAPPs, and develop multiple EOS-based services."
+    }
+  },
+  {
+    bp: "eos42freedom",
+    general_info: {
+      websiteText:
+        "EOS42 is a consultancy team for EOSIO applications and block production. We run nodes on all major EOSIO blockchains, build DeFi applications, provide trusted smart contract audits and advise projects on everything from technical development to tokenomic design."
+    }
+  },
+  {
+    bp: "eoscannonchn",
+    general_info: {
+      websiteText:
+        "As the most effective community with well-known credibility, we are willing to connect and promote sharing between overseas and Chinese EOS communities. We are happy to lead the majority of domestic token holders to participate in global EOS community growth."
+    }
+  },
+  {
+    bp: "eosswedenorg",
+    general_info: {
+      websiteText:
+        "EOS sw/eden and its team members main priority is in building blocks for the Blockchain. Maintaining and upgrading infrastructure and security will always be the first priority because without safe block production there is no chain. Our system is designed to scale up and down quickly and securely."
+    }
+  },
+  {
+    bp: "eosauthority",
+    general_info: {
+      websiteText:
+        "Official EOS block Explorer and Wallet of EOS Authority. Search EOS transactions, blocks, accounts, tokens, rex, prices with alerts."
+    }
+  },
+  {
+    bp: "eosriobrazil",
+    general_info: {
+      websiteText:
+        "We believe EOS has the potential to positively impact the lives of billions of people around the world through scalable, secure and robust distributed applications. EOS Rio team is committed to contribute its best to fulfill EOS potential."
+    }
+  },
+  {
+    bp: "argentinaeos",
+    general_info: {
+      websiteText:
+        "EOS Argentina's efforts focus in encouraging the use of the EOS Blockchain. To accomplish his task we will work on the following objectives: Community Building, EOS Tech Promotion, Dapps Support, Infrastructure."
+    }
+  },
+  {
+    bp: "cochainworld",
+    general_info: {
+      websiteText:
+        "EOS Cochain is a homogeneous parallel chain of EOS, helping EOS connect the multi-chain interconnected ecosystem. As the first parallel chain that can interoperate with the EOS main chain, EOS Cochain strives to achieve safe, reliable, fast and convenient cross-chain asset transfer and cross-chain smart contract invocation. Anyone can develop or use cross-chain DAPP on the EOS Cochain parallel chain."
+    }
+  },
+  {
+    bp: "eosbeijingbp",
+    general_info: {
+      websiteText:
+        "In recent years, the development of blockchain technology has advanced faster than its practical applications. The balance of EOS performance and decentralization has led us to see the possibility of bridging this gap. The EOS Beijing team is composed of experienced digital currency professionals and investors who are eager to participate in this new cutting-edge technology."
+    }
+  },
+  {
+    bp: "eoscanadacom",
+    general_info: {
+      websiteText:
+        "EOS Canada is a top global EOS block producer. Leading through technology, EOS Canada is founded by a team of serial tech entrepreneurs who is deeply committed to contribute to the growth of the EOS ecosystem."
+    }
+  },
+  {
+    bp: "eos.fish",
+    general_info: {
+      websiteText:
+        "Our mission is to ensure the success of the EOS ecosystem and build a decentralized future together. Bridging the gap between blockchain protocols is an important part of working towards this mission. The first step is to grow the EOS community by attracting active community members, including project teams, developers, voters, and end users."
+    }
+  },
+  {
+    bp: "eosnationftw",
+    general_info: {
+      websiteText:
+        "Our mission is to secure EOSIO blockchains with reliable infrastructure and to build a global community equipped with the tools and resources to thrive. We’re committed to leading and contributing to projects that add value to the ecosystem."
+    }
+  },
+  {
+    bp: "cypherglasss",
+    general_info: {
+      websiteText:
+        "Cypherglass is an independent blockchain infrastructure company. We design, build, and maintain infrastructure for the world’s leading blockchains, like EOS and the DAPP Network. Our mission is to increase adoption of decentralized blockchain technologies and help enable a more distributed world."
+    }
+  },
+  {
+    bp: "eossv12eossv",
+    general_info: {
+      websiteText:
+        "Providing world class Silicon Valley talent to oversee, govern and maximize growth to the EOS ecosystem. We act as a leading block producer, staying ahead of the curve by providing infrastructure that scales and community & growth focused governance. We act as as EOS leaders by supporting the community through education and incubating decentralized applications for the masses."
+    }
+  },
+  {
+    bp: "teamgreymass",
+    general_info: {
+      websiteText:
+        "We are TeamGreymass! Our world-class block production infrastructure ensures that your data is always secure, and available when you need it. Block rewards not only power our infrastructure, but also fuel the development of decentralized applications, services, and frameworks. These projects enrich blockchain ecosystems and deepen the engagement of creators and regular users alike."
+    }
+  },
+  {
+    bp: "eostitanprod",
+    general_info: {
+      websiteText:
+        "EOS Titan is a group of individuals involved in software development, telecommunications, encryption and blockchain technologies. Our team is composed of early adopters of cryptocurrencies who share the ethos of the ecosystem and we are looking forward to bringing the industry one step further towards the goal of massively decentralized governance and financial structures."
+    }
+  },
+  {
+    bp: "eosdacserver",
+    general_info: {
+      websiteText:
+        "It is the vision of eosDAC that EOS.IO block production should be open for everyone to contribute and benefit. To realise this vision, eosDAC is an evolving Decentralised Autonomous Community (DAC) focused on EOS.IO Block Production serving the EOS communities worldwide. In doing this, eosDAC is creating the tools and smart contracts it needs to function. It will share these with the EOS communities to help other DACs thrive on the EOS.IO blockchains."
+    }
+  },
+  {
+    bp: "libertyblock",
+    general_info: {
+      websiteText:
+        "LibertyBlock is an EOS Block Producer focused on delivering security and technical expertise to the ecosystem."
+    }
+  },
+  {
+    bp: "eospacificbp",
+    general_info: {
+      websiteText:
+        "Node Pacific is The Heart of the Blockchain, The community Governance Organizer, The Technical Solutions Provider and The Link of the Blockchain Eco-System"
+    }
+  },
+  {
+    bp: "eosasia11111",
+    general_info: {
+      websiteText:
+        "We will bring world-class technical expertise and international representation to make EOS the ultimate blockchain infrastructure for real applications. More importantly, our goal is to keep EOS a healthy community-driven decentralized prototol, and we will fight anyone who aims to privatize the ecosystem for their own benefit."
+    }
+  },
+  {
+    bp: "cryptolions1",
+    general_info: {
+      websiteText: "Building our decentralized future, one block at a time. EOSIO block producer based in Ukraine."
+    }
+  },
+  {
+    bp: "eospaceioeos",
+    general_info: {
+      websiteText:
+        "With the leading blockchain, sidechain, dapp, and contract developing ability, EOSpace is committed to be the best BP. EOSpace set up and maintain dozens of communities of EOS. We have built a distributed blockchain lab, including related business like traffic center, news center, overseas operation, investment banking service, project center and exchange center(Australia, Singapore, Korea) etc. We hope to continue to contribute to the EOS ecosystem."
+    }
+  },
+  {
+    bp: "eosinfstones",
+    general_info: {
+      websiteText:
+        "Infinity Stones is a silicon valley-based company, providing cloud management services and security solutions for Blockchains. We are blockchain infrastructure architects with proven track records in the industry. Our team is composed of tech leads, management experts, and financial professionals from tech giants like Google, Oracle, and Intel."
+    }
+  },
+  {
+    bp: "oraclegogogo",
+    general_info: {
+      websiteText:
+        "The secure random number scheme is a commercial-level random number solution initiated by OracleChain, which is provided by multiple trusted organizations to generate random numbers. Any third-party applications (smart contract) can easily get random numbers by subscribing random service."
+    }
+  },
+  {
+    bp: "eostribeprod",
+    general_info: {
+      websiteText:
+        "EOS Tribe, LLC is a Blockchain company that designs and develops technological solutions for the latest Blockchain smart platforms to solve challenges for real World businesses and serve as catalyst for mass adoption of Blockchain technology Worldwide."
+    }
+  },
+  {
+    bp: "eosyskoreabp",
+    general_info: {
+      websiteText: ""
+    }
+  },
+  {
+    bp: "eosgenblockp",
+    general_info: {
+      websiteText:
+        "A mining as a service company who built the first mining farm in Eastern Europe. Since our founding, we have grown tremendously and a lot has happened, but one thing remains constant: We are all strong believers in the future of digital currencies and we love being part of this growing community."
+    }
+  },
+  {
+    bp: "eosiomeetone",
+    general_info: {
+      websiteText:
+        "MEET.ONE Chain is designed to facilitate the liquidity of everyone’s values，lower the threshold and cost of access to EOSIO layer. Meanwhile, MEET.ONE Chain is committed to make a joint effort with Block Producers and developers, aiming to promote the development of trustworthy Internet and utilize blockchain technology to empower Internet."
+    }
+  },
+  {
+    bp: "eosrapidprod",
+    general_info: {
+      websiteText:
+        "EOS Rapid is a next generation block producer candidate looking to provide both transparency and performance to the EOS community."
+    }
+  },
+  {
+    bp: "eoslaomaocom",
+    general_info: {
+      websiteText:
+        "EOSLaoMao is an EOS Block Producer dedicated to maintaining a fair and stable BP node. We aim to maximize the potential of EOS with the dynamic community around the world."
+    }
+  },
+  {
+    bp: "eosliquideos",
+    general_info: {
+      websiteText:
+        "LiquidEOS is built by passionate blockchain entrepreneurs who believe that the EOS blockchain is pushing the envelope of innovation and will usher in an era of mass user adoption of decentralized applications. We are an R&D center, an EOS Block Producer, & a think tank that’s dedicated to enhancing the EOS network through new ideas, tools, & content. We develop critical EOS infrastructure, build scalable decentralized technologies & engage the global EOS ecosystem through a range of community building initiatives."
+    }
+  }
+];
