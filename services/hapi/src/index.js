52ecf25e3df25b7ed720f3589829d2cb8d526132
Xavier Fernandez <xavier@crservers.com>
2020-02-29 13:54:34 -0600

    Add hasura actions (#313)
    * add hasura actions

--- services/hapi/src/index.js
+++ services/hapi/src/index.js
@@ -21,20 +21,22 @@ const init = async () => {
   })
 
   server.route({
-    method: 'GET',
+    method: 'POST',
     path: '/ratebp',
-    handler: request => {
-      const bp = request.query.producer
+    handler: req => {
+      console.log(req.payload.input.ratingInput)
+      const bp = req.payload.input.ratingInput.producer
       if (bp) {
         updateBpStats(bp)
       }
-
-      const user = request.query.user
+      const user = req.payload.input.ratingInput.user
       if (user) {
         updateUserRatings(user)
       }
 
-      return 'updating stats for producer: ' + bp + ' user : ' + user
+      return {
+        message: 'updating stats for producer: ' + bp + ' user : ' + user
+      }
     }
   })
 
