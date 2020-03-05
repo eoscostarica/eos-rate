c63d669cd10fb2db3e8758ca77313edbcfe1a90f
Xavier Fernandez <xavier@crservers.com>
2020-03-05 17:51:58 -0600

    refactor(FE): add user account info and set minimum value rate (#323)
    * refactor(FE): add user account info and set minimum value rate
    
    * refactor(FE): yarn lint errors

--- services/frontend/src/routes/account/index.js
+++ services/frontend/src/routes/account/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
 import Button from '@material-ui/core/Button'
@@ -32,8 +32,27 @@ const style = theme => ({
   }
 })
 
-const Account = ({ classes }) => {
+const Account = ({ classes, ual }) => {
   const { t } = useTranslation('account')
+  const [accountInfo, setAccountInfo] = useState({
+    accountName: null,
+    accountBalance: null
+  })
+
+  useEffect(() => {
+    async function getAccountInfo () {
+      const accountName = await ual.activeUser.getAccountName()
+      const account = await ual.activeUser.rpc.get_account(accountName)
+      const accountBalance = account.core_liquid_balance
+
+      setAccountInfo({
+        accountName,
+        accountBalance
+      })
+    }
+
+    ual.activeUser && getAccountInfo()
+  }, [ual.activeUser])
 
   return (
     <Grid container className={classes.container}>
@@ -43,19 +62,13 @@ const Account = ({ classes }) => {
             {t('title')}
           </Typography>
           <Grid className={classes.box}>
-            {/* {entries.map(entry => (
-              <Typography
-                key={entry[0]}
-                variant='subtitle1'
-                className={classes.bold}
-              >
-                {t(entry[0])}: {`${entry[1]}`}
-              </Typography>
-            ))} */}
+            <Typography variant='subtitle1' className={classes.bold}>
+              {accountInfo.accountName}: {accountInfo.accountBalance || 0}
+            </Typography>
             <Button
               className={classes.button}
               color='secondary'
-              onClick={() => console.log('logout')}
+              onClick={() => ual.logout()}
               variant='outlined'
             >
               {t('logout')}
@@ -68,7 +81,8 @@ const Account = ({ classes }) => {
 }
 
 Account.propTypes = {
-  classes: PropTypes.object
+  classes: PropTypes.object,
+  ual: PropTypes.object
 }
 
 export default withStyles(style)(Account)
