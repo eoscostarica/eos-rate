24d75145eff7a1046c7a92d49c1fa64abeaa1c77
Teto Gomez <terencio17@rocketmail.com>
2020-05-03 23:39:42 -0600

    feat(FE):add ual anchor (#411)
    Co-authored-by: Teto Gomez <macbookproretina@MacBooks-MacBook-Pro.local>

--- services/frontend/src/utils/ualAuthenticators.js
+++ services/frontend/src/utils/ualAuthenticators.js
@@ -3,6 +3,7 @@ import { Ledger } from 'ual-ledger'
 import { Lynx } from 'ual-lynx'
 import { TokenPocket } from 'ual-token-pocket'
 import { MeetOne } from 'ual-meetone'
+import { Anchor } from 'ual-anchor'
 
 import config from 'config'
 
@@ -26,5 +27,13 @@ const ledger = new Ledger([network])
 const tokenPocket = new TokenPocket([network.chainId])
 const meetOne = new MeetOne([network.chainId])
 const scatter = new Scatter([network], { appName })
+const anchor = new Anchor([network], { appName })
 
-export const authenticators = [lynx, ledger, scatter, tokenPocket, meetOne]
+export const authenticators = [
+  lynx,
+  ledger,
+  scatter,
+  tokenPocket,
+  meetOne,
+  anchor
+]
