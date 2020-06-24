4a32c47ed7ec7292d6be24a48bd95e3fd90f9d23
kurono <kurono@riseup.net>
2020-06-24 12:56:29 +0000

    add a script to stop the kubernetes services.
    refs #420.

---
+++ scripts/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: clean.sh <environment>"
+    exit 0
+fi
+
+kubectl delete --namespace ${1} deployment demux hapi \
+                 hasura pgweb postgres frontend
+kubectl delete --namespace ${1} svc demux hapi hasura \
+                 pgweb postgres frontend frontend-port-http \
+                 frontend-port-https hasura-port
+kubectl delete --namespace ${1} networkpolicy default
