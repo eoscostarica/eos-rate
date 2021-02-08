dfeb862485bfab6a6088e8cc6fe190c2002d15dd
Andres Gomez <kurono@riseup.net>
2021-02-08 20:49:05 +0000

    Migrate eos-rate to the k3s cluster (#492)
    * start cleaning eos-rate repo for migration to k3s. refs #491
    
    * create statefulset for the eos-rate app. refs #491
    
    * create makefiles. refs #491
    
    * create workflow actions. refs #491
    
    * depurate demux and cronjob code. refs #491
    
    * improve configuration and secrets. refs #491
    
    * fix staging container. refs #491
    
    * fix the container image names. refs #491
    
    * depurate frontend service. refs #491
    
    * clean k8s repo. refs #491
    
    * Fix the used hasura port. refs #491
    
    * fix frontend compilation problem. refs #491
    
    ref: https://stackoverflow.com/questions/62663167/dockerizing-react-in-production-mode-fatal-error-ineffective-mark-compacts-nea
    
    * fix grpahql secure endpoints. refs #491
    
    * change hasura grapql DNS. refs #491
    
    * fix type in migrations path. refs #491
    
    * delete unnecessary reference to nfs mount. refs #491
    
    * fix the container version. refs #491
    
    * improve staging environment action workflow. refs #491
    
    * improve production environment action workflow. refs #491

---
+++ utils/meta.mk
@@ -0,0 +1,19 @@
+-include .env
+
+VERSION ?= $(shell git rev-parse --short HEAD)
+CURRENT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
+
+IMAGE_NAME_WEBAPP=frontend
+IMAGE_NAME_HAPI=hapi
+IMAGE_NAME_HASURA=hasura
+
+DOCKER_REGISTRY=eoscostarica506
+SUBDIRS = frontend hapi hasura
+
+MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME_WEBAPP IMAGE_NAME_HAPI IMAGE_NAME_HASURA
+
+SHELL_EXPORT := $(foreach v,$(MAKE_ENV),$(v)='$($(v))')
+
+ifneq ("$(wildcard .env)", "")
+	export $(shell sed 's/=.*//' .env)
+endif
