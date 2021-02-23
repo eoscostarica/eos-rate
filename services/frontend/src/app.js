9716b78a874bcb0936a249e9445f6e23843f2665
Teto Gomez <terencio17@rocketmail.com>
2021-02-23 16:05:17 -0600

    fix(FE): remove HoCs

--- services/frontend/src/app.js
+++ services/frontend/src/app.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
 import { Router } from '@reach/router'
 
 import Spinner from 'components/spinner'
@@ -8,31 +10,27 @@ import Layout from 'components/layout'
 import NotFound from 'routes/not-found'
 import routes from 'routes'
 
-const App = ({ isContentLoading, ual }) => (
-  <>
-    <Spinner isLoading={isContentLoading} />
-    <Layout ual={ual}>
-      <Router>
-        {routes.map(({ path, Component }) => (
-          <Component key={`path-${path}`} path={path} ual={ual} />
-        ))}
-        <NotFound default />
-      </Router>
-    </Layout>
-  </>
-)
+const App = ({ ual }) => {
+  // const dispatch = useDispatch()
+  const { isContentLoading } = useSelector((state) => state.isLoading)
+
+  return (
+    <>
+      <Spinner isLoading={isContentLoading} />
+      <Layout ual={ual}>
+        <Router>
+          {routes.map(({ path, Component }) => (
+            <Component key={`path-${path}`} path={path} ual={ual} />
+          ))}
+          <NotFound default />
+        </Router>
+      </Layout>
+    </>
+  )
+}
 
 App.propTypes = {
-  isContentLoading: PropTypes.bool,
   ual: PropTypes.object
 }
 
-App.defaultProps = {
-  isContentLoading: false
-}
-
-const mapStateToProps = state => ({
-  isContentLoading: state.isLoading.isContentLoading
-})
-
-export default connect(mapStateToProps, null)(App)
+export default App
