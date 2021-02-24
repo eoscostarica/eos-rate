244d54b89ecd8c3a9e596d28d38cf3c6cffbd7cf
Teto Gomez <terencio17@rocketmail.com>
2021-02-23 19:02:56 -0600

    Update project dependencies (#504)
    * fix(FE): update dependencies
    
    * fix(FE): remove eslint config options

--- services/frontend/src/store.js
+++ services/frontend/src/store.js
@@ -1,10 +1,13 @@
 import { init } from '@rematch/core'
 import createPersistPlugin from '@rematch/persist'
+import storage from 'redux-persist/lib/storage'
 
 import { locationChangeListener } from 'models/location'
 import * as models from 'models'
 
 const persistPlugin = createPersistPlugin({
+  key: 'root',
+  storage,
   whitelist: ['blockProducers', 'settings']
 })
 
