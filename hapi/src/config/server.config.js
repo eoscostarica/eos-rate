a687d39a4ba0d2156563a802024b6d5a7d0f8fb4
Xavier <5632966+xavier506@users.noreply.github.com>
2021-07-30 21:19:57 -0600

    fix(devops): remove pgwebservice from productiony (#631)
    * fix(devops): remove pgwebservice from productiony
    
    * fix(devops): adjust env var values

--- hapi/src/config/server.config.js
+++ hapi/src/config/server.config.js
@@ -3,6 +3,6 @@ module.exports = {
   host: process.env.HAPI_POSTGRES_HOST || 'postgres',
   port: process.env.HAPI_POSTGRES_PORT || 5432,
   database: process.env.HAPI_POSTGRES_DB || 'eosrate',
-  user: process.env.HAPI_POSTGRES_USER || 'user',
+  user: process.env.HAPI_POSTGRES_USER || 'eoscr',
   password: process.env.HAPI_POSTGRES_PASSWORD || 'password'
 }
