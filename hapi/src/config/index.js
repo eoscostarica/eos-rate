df35023bbe9731efbab23c42a7419dceeffcb9c0
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-08-15 03:52:54 -0600

    feat(database): create eden__atings_stats table

--- hapi/src/config/index.js
+++ hapi/src/config/index.js
@@ -2,8 +2,10 @@ const massive = require('massive')
 
 const massiveConfig = require('./server.config')
 const massiveDB = (async () => await massive(massiveConfig))()
+const CONTRACT_SCOPES = [process.env.HAPI_RATING_CONTRACT, "genesis.eden"]
 
 module.exports = {
     massiveConfig,
-    massiveDB
+    massiveDB,
+    CONTRACT_SCOPES
 }
