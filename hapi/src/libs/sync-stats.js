a5d4f529d4f62fe6f7d79b35292a19520fdd1f8a
Leister Alvarez Campos <leisteralvarezcampos@Leisters-MacBook-Pro.local>
2021-08-25 16:23:35 -0600

    refactor(sync): get eden scope data from cronjob query

--- hapi/src/libs/sync-stats.js
+++ hapi/src/libs/sync-stats.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 const { JsonRpc } = require('eosjs')
 const fetch = require('node-fetch')
-const { massiveDB } = require('../config')
+const {
+  defaultContractScope,
+  edenContractScope,
+  massiveDB
+} = require('../config')
 
 const HAPI_EOS_API_ENDPOINT = process.env.HAPI_EOS_API_ENDPOINT || 'https://jungle.eosio.cr'
 const HAPI_RATING_CONTRACT = process.env.HAPI_RATING_CONTRACT || 'rateproducer'
 
-const getRatingsStats = async () => {
+const getRatingsStats = async (scope) => {
   const eos = new JsonRpc(HAPI_EOS_API_ENDPOINT, { fetch })
 
   try {
     const ratings = await eos.get_table_rows({
       json: true,
       code: HAPI_RATING_CONTRACT,
-      scope: HAPI_RATING_CONTRACT,
+      scope: scope,
       table: 'stats',
       limit: 1000,
       reverse: false,
@@ -29,13 +33,22 @@ const getRatingsStats = async () => {
 
 const updateRatingsStats = async () => {
   console.log('==== Updating ratings stats ====')
-  const ratingsStats = await getRatingsStats()
+  const defaultRatingsStats = await getRatingsStats(defaultContractScope)
+  const edenRatingsStats = await getRatingsStats(edenContractScope)
 
-  ratingsStats.rows.forEach(async (rating) => {
+  defaultRatingsStats.rows.forEach(async (rating) => {
     try {
       const resultRatingStatsSave = await (await massiveDB).ratings_stats.save(rating)
       const dbResult = resultRatingStatsSave ? resultRatingStatsSave : await (await massiveDB).ratings_stats.insert(rating)
-      console.log(`Save or insert of ${rating.bp} was ${dbResult ? 'SUCCESSFULL' : 'UNSUCCESSFULL'}`)
+      console.log(`Default save or insert of ${rating.bp} was ${dbResult ? 'SUCCESSFULL' : 'UNSUCCESSFULL'}`)
+    } catch (err) { console.log(`Error: ${err}`) }
+  })
+
+  edenRatingsStats.rows.forEach(async (rating) => {
+    try {
+      const resultRatingStatsSave = await (await massiveDB).eden_ratings_stats.save(rating)
+      const dbResult = resultRatingStatsSave ? resultRatingStatsSave : await (await massiveDB).eden_ratings_stats.insert(rating)
+      console.log(`Eden save or insert of ${rating.bp} was ${dbResult ? 'SUCCESSFULL' : 'UNSUCCESSFULL'}`)
     } catch (err) { console.log(`Error: ${err}`) }
   })
 }
