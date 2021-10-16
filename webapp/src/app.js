8dfb08b4ef6390025510810d03e17eb83b1f4bfe
Teto Gomez <terencio17@rocketmail.com>
2021-10-15 19:54:24 -0600

    fix(webapp): overwrite styles (#840)
    * fix(webapp): overwrite styles

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -2,6 +2,7 @@ import React, { Suspense, useMemo } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import { StylesProvider, createGenerateClassName } from '@mui/styles'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 
@@ -11,6 +12,10 @@ import DashboardLayout from './layouts/Dashboard'
 import { useSharedState } from './context/state.context'
 import getTheme from './theme'
 import './i18n'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'eosRate'
+})
 
 const App = () => {
   const [state] = useSharedState()
@@ -31,16 +36,18 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DashboardLayout routes={userRoutes.sidebar}>
-            <Suspense fallback={<Loader />}>
-              <Switch>{userRoutes.browser.map(renderRoute)}</Switch>
-            </Suspense>
-          </DashboardLayout>
-        </LocalizationProvider>
-      </ThemeProvider>
+      <StylesProvider generateClassName={generateClassName}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DashboardLayout routes={userRoutes.sidebar}>
+              <Suspense fallback={<Loader />}>
+                <Switch>{userRoutes.browser.map(renderRoute)}</Switch>
+              </Suspense>
+            </DashboardLayout>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
