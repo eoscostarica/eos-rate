b40c4de4319ee073bf72e49efe5d68bbd056efae
Xavier <5632966+xavier506@users.noreply.github.com>
2021-12-01 12:29:39 -0600

    Revert "New home page (#862)" (#872)
    This reverts commit 4ec2113dcf592c12ec7490cea2ca1bfc429e8fc1.

--- webapp/src/context/state.context.js
+++ webapp/src/context/state.context.js
@@ -27,8 +27,7 @@ const initialValue = {
   proxies: { data: [], rows: 0 },
   selectedProxies: [],
   proxy: null,
-  compareProxyToolVisible: false,
-  homeProducers: []
+  compareProxyToolVisible: false
 }
 
 const sharedStateReducer = (state, action) => {
@@ -138,12 +137,6 @@ const sharedStateReducer = (state, action) => {
       return {
         ...state,
         loadingLogin: action.loading
-      }
-
-    case 'setHomeProducers':
-      return {
-        ...state,
-        homeProducers: action.homeProducers
       }
 
     default: {
@@ -289,38 +282,6 @@ export const useSharedState = () => {
     dispatch({ type: 'setProducer', blockProducer })
   }
 
-  const setHomeProducers = async bpList => {
-    let homeProducers = bpList
-
-    if (!homeProducers) {
-      homeProducers = await getProducers(3, [{ total_votes: 'desc' }])
-    }
-
-    const allBps = []
-
-    homeProducers.data.forEach(bp => {
-      const totalStats = getTotalStats({
-        producerData: {
-          ...bp?.system?.parameters,
-          average: bp?.average,
-          ratings_cntr: bp?.ratings_cntr
-        },
-        edenStats: bp?.edenRate,
-        statsAmount: 5,
-        oneStat: 1
-      })
-      bp = {
-        ...bp,
-        totalStats
-      }
-      allBps.push(bp)
-    })
-
-    homeProducers = { ...homeProducers, data: allBps }
-
-    dispatch({ type: 'setHomeProducers', homeProducers })
-  }
-
   const handleMutationInsertUserRating = async ({
     ual,
     user,
@@ -409,8 +370,7 @@ export const useSharedState = () => {
       setProxies,
       setProxy,
       setCompareProxyTool,
-      setSelectedProxies,
-      setHomeProducers
+      setSelectedProxies
     }
   ]
 }
