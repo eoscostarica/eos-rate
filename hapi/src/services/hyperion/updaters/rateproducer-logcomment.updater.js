06bfd8182c95c0ae1c376424d8f0c1df56393bbd
Angelo CG <angelocas13_8@hotmail.com>
2022-04-25 08:26:56 -0600

    fix(hyperion): fix codefactor issues

--- hapi/src/services/hyperion/updaters/rateproducer-logcomment.updater.js
+++ hapi/src/services/hyperion/updaters/rateproducer-logcomment.updater.js
@@ -48,7 +48,7 @@ module.exports = {
       }
 
       const [blockProducer] = userRatings.rows.filter(
-        ({ id }) => id == ratingId
+        ({ id }) => id === ratingId
       )
 
       await save({
