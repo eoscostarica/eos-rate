741820984c28d428a49ca69da8a59a0f1c457551
hanakoori <paola.espinoza.cascante@est.una.ac.cr>
2022-07-22 14:22:51 -0600

    (webapp): fix header change position on homepage

--- hapi/src/config/server.config.js
+++ hapi/src/config/server.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: process.env.HAPI_POSTGRES_HOST || 'postgres',
+  host: 'localhost',
   port: process.env.HAPI_POSTGRES_PORT || 5432,
   database: process.env.HAPI_POSTGRES_DB || 'eosrate',
-  user: process.env.HAPI_POSTGRES_USER || 'eoscr',
+  user: process.env.HAPI_POSTGRES_USER || 'user',
   password: process.env.HAPI_POSTGRES_PASSWORD || 'password'
 }
